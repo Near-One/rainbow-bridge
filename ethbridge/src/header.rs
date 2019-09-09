a612cbe0412a5c49598bbbbf7de8d1f3c8ba2042
Anton Bukov <k06aaa@gmail.com>
2019-09-09 18:05:48 +0300

    Add RLP decoder for Eth BlockHeader

---
+++ ethbridge/src/header.rs
@@ -0,0 +1,60 @@
+extern crate crypto;
+
+use rlp::{Rlp, DecoderError, Decodable};
+use ethereum_types::{H256, U256, Address, Bloom};
+use self::crypto::digest::Digest;
+use self::crypto::sha3::Sha3;
+
+#[derive(Debug, Clone)]
+pub struct BlockHeader {
+    pub parent_hash: H256,
+    pub timestamp: u64,
+    pub number: u64,
+    pub author: Address,
+
+    pub transactions_root: H256,
+    pub uncles_hash: H256,
+    pub extra_data: Vec<u8>,
+
+    pub state_root: H256,
+    pub receipts_root: H256,
+    pub log_bloom: Bloom,
+    pub gas_used: U256,
+    pub gas_limit: U256,
+
+    pub difficulty: U256,
+
+    pub hash: H256,
+}
+
+fn keccak256(data: &[u8]) -> [u8; 32] {
+    let mut hasher = Sha3::keccak256();
+    hasher.input(data);
+
+    let mut buffer = [0u8; 32];
+    hasher.result(&mut buffer);
+    buffer
+}
+
+impl Decodable for BlockHeader {
+	fn decode(rlp: &Rlp) -> Result<Self, DecoderError> {
+		let blockheader = BlockHeader {
+			parent_hash: rlp.val_at(0)?,
+			uncles_hash: rlp.val_at(1)?,
+			author: rlp.val_at(2)?,
+			state_root: rlp.val_at(3)?,
+			transactions_root: rlp.val_at(4)?,
+			receipts_root: rlp.val_at(5)?,
+			log_bloom: rlp.val_at(6)?,
+			difficulty: rlp.val_at(7)?,
+			number: rlp.val_at(8)?,
+			gas_limit: rlp.val_at(9)?,
+			gas_used: rlp.val_at(10)?,
+			timestamp: rlp.val_at(11)?,
+			extra_data: rlp.val_at(12)?,
+			hash: keccak256(rlp.as_raw()).into(),
+		};
+
+		Ok(blockheader)
+	}
+}
--- ethbridge/src/lib.rs
+++ ethbridge/src/lib.rs
@@ -1,26 +1,50 @@
 #![feature(const_vec_new)]
+
+extern crate crypto;
+extern crate rlp;
+
 use std::collections::HashMap;
 use borsh::{BorshDeserialize, BorshSerialize};
-use near_bindgen::{near_bindgen, env};
+use near_bindgen::{near_bindgen};
+
+mod header;
+use header::{BlockHeader};
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct StatusMessage {
-    records: HashMap<String, String>,
+pub struct EthBridge {
+    block_hashes: HashMap<u64, [u8; 32]>,
+    last_block_number: u64
 }
 
 #[near_bindgen]
-impl StatusMessage {
-    pub fn set_status(&mut self, message: String) {
-        let account_id = env::signer_account_id();
-        self.records.insert(account_id, message);
+impl EthBridge {
+    pub fn add_block_headers(&mut self, start: u64, block_headers: Vec<Vec<u8>>) {
+        let mut best_block_number = 0u64;
+        for i in start..(start + block_headers.len() as u64) {
+            match rlp::decode::<BlockHeader>(block_headers[i as usize].as_slice()) {
+                Ok(block_header) => {
+                    assert!(block_header.number == self.last_block_number + 1);
+                    match self.block_hash(self.last_block_number) {
+                        Some(hash) => assert_eq!(block_header.parent_hash, hash.into()),
+                        None => {},
+                    }
+                    self.block_hashes.insert(i, block_header.hash.into());
+                    best_block_number = block_header.number;
+                },
+                Err(_e) => {
+                    panic!();
+                },
+            }
+        }
+        self.last_block_number = best_block_number;
     }
 
-    pub fn get_status(&self, account_id: String) -> Option<String> {
-        self.records.get(&account_id).cloned()
+    pub fn block_hash(&self, index: u64) -> Option<[u8; 32]> {
+        self.block_hashes.get(&index).cloned()
     }
 }
 
