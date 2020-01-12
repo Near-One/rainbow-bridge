0b07c91a768d03a6fe4c78fc307633e6e7c1c178
Anton Bukov <k06aaa@gmail.com>
2020-01-12 23:49:22 +0300

    Update docker image name

--- ethrelay/scripts/start.sh
+++ ethrelay/scripts/start.sh
@@ -22,9 +22,8 @@ nearnode_running() {
 }
 
 start_nearnode() {
-    echo "ethrelay" | "$DIR/start_localnet.py" --home "$DIR/.near" --image "nearprotocol/nearcore:0.4.12"
-    nearnode_pid=$!
-    sleep 5
+    echo "ethrelay" | "$DIR/start_localnet.py" --home "$DIR/.near" --image "nearprotocol/nearcore:withfloats"
+    sleep 10
 }
 
 if nearnode_running; then
