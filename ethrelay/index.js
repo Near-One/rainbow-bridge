364144e98004da0be271bad5bc7b939808512c16
Anton Bukov <k06aaa@gmail.com>
2020-01-13 17:53:29 +0300

    Add some checks before submitting

--- ethrelay/index.js
+++ ethrelay/index.js
@@ -58,13 +58,24 @@ function subscribeOnBlocksRangesFrom(web3, block_number, handler) {
     }
 
     subscribeOnBlocksRangesFrom(web3, last_block_number, async (start, stop) => {
-        console.log(start, stop);
-
-        const blocks = [];
+        let blocks = [];
         for (let i = start; i <= stop; i++) {
             console.log(`Computing for block #${i}`)
             const res = await execute(`./ethashproof/cmd/relayer/relayer ${i} | sed -e '1,/Json output/d'`);
             blocks.push(JSON.parse(res));
+        }
+
+        // Check bridge state, may be changed since computation could be long
+        const last_block_number_onchain = await ethBridgeContract.last_block_number();
+        console.log('ethBridgeContract.last_block_number =', last_block_number_onchain);
+        if (last_block_number_onchain >= stop) {
+            console.log('Skipping submission due all were already sumbitted by someone');
+            return;
+        }
+        if (last_block_number_onchain > start) {
+            console.log('Trim first ${last_block_number_onchain - start} headers were due already submitted by someone');
+            blocks = blocks.slice(last_block_number_onchain - start);
+            start = last_block_number_onchain;
         }
 
         console.log(`Submiting ${blocks.length} blocks from ${start} to ${stop} to EthBridge`);
@@ -88,34 +99,6 @@ function subscribeOnBlocksRangesFrom(web3, block_number, handler) {
                         };
                     });
             })
-
-            // pub fn to_double_node_with_merkle_proof_vec(&self) -> Vec<DoubleNodeWithMerkleProof> {
-            //     let h512s = Self::combine_dag_h256_to_h512(self.elements.clone());
-            //     h512s.iter().zip(h512s.iter().skip(1)).enumerate().filter(|(i,_)| {
-            //         i % 2 == 0
-            //     }).map(|(i,(a,b))| {
-            //         DoubleNodeWithMerkleProof {
-            //             dag_nodes: vec![*a, *b],
-            //             proof: self.merkle_proofs[i/2 * self.proof_length as usize .. (i/2 + 1) * self.proof_length as usize].to_vec(),
-            //         }
-            //     }).collect()
-            // }
-
-            // pub struct DoubleNodeWithMerkleProof {
-            //     pub dag_nodes: Vec<H512>, // [H512; 2]
-            //     pub proof: Vec<H128>,
-            // }
-
-            // fn combine_dag_h256_to_h512(elements: Vec<H256>) -> Vec<H512> {
-            //     elements.iter().zip(elements.iter().skip(1)).enumerate().filter(|(i,_)| {
-            //         i % 2 == 0
-            //     }).map(|(_,(a,b))| {
-            //         let mut buffer = [0u8; 64];
-            //         buffer[..32].copy_from_slice(&(a.0).0);
-            //         buffer[32..].copy_from_slice(&(b.0).0);
-            //         H512(buffer.into())
-            //     }).collect()
-            // }
         });
     });
 
