8dad2d05a28fed8329a04203e62206c035b6db70
Anton Bukov <k06aaa@gmail.com>
2020-02-13 03:06:47 +0300

    WIP

--- ethprover/src/lib.rs
+++ ethprover/src/lib.rs
@@ -121,29 +121,33 @@ impl EthProver {
         }
     }
 
+    fn matchingNibbleLength(a: Vec<u8>, b: Vec<u8>) -> usize {
+        a.iter().zip(b.iter()).take_while(|(i,j)| i == j).count()
+    }
+
     /// https://github.com/slockit/in3/blob/master/src/util/merkleProof.ts
     pub fn verify_log_entry(
         &self,
         log_entry_data: Vec<u8>,
         receipt_data: Vec<u8>,
         header_data: Vec<u8>,
-        receipt_proof: Vec<H256>,
+        proof: Vec<H256>,
         receipt_index_hash: H256,
     ) -> bool {
         let log_entry: LogEntry = rlp::decode(log_entry_data.as_slice()).unwrap();
         let receipt: Receipt = rlp::decode(receipt_data.as_slice()).unwrap();
-        let header_data: BlockHeader = rlp::decode(header_data.as_slice()).unwrap();
+        let header: BlockHeader = rlp::decode(header_data.as_slice()).unwrap();
 
         // create the nibbles to iterate over the path
         let mut key = bytesToNibbles((receipt_index_hash.0).0.to_vec());
 
         // start with the root-Hash
-        let mut wantedHash = header_data.receipts_root;
+        let mut wantedHash = header.receipts_root;
         let mut lastNode: Option<EthTrieNode> = Option::None;
 
         // iterate through the nodes starting at root
-        for i in 0..receipt_proof.len() {
-            let p = receipt_proof[i];
+        for i in 0..proof.len() {
+            let p = proof[i];
             let hash = near_keccak256(&(p.0).0);
 
             // verify the hash of the node
@@ -161,7 +165,7 @@ impl EthProver {
                 EthTrieNodeType::Branch => {
                     // we reached the end of the path
                     if key.len() == 0 {
-                        return i == receipt_proof.len() - 1 && node.value == receipt_data;
+                        return i == proof.len() - 1 && node.value == receipt_data;
                     }
 
                     // find the childHash
@@ -169,11 +173,74 @@ impl EthProver {
                     // remove the first item
                     key = key.iter().skip(1).cloned().collect();
 
-                    //TODO: WIP
-                }
+                    if childHash.len() == 2 {
+                        let embeddedNode: EthTrieNode = rlp::decode(childHash.as_slice()).unwrap();
+                        if i != proof.len() - 1 {
+                            panic!("Additional nodes at end of proof (embeddedNode)");
+                        }
+
+                        if Self::matchingNibbleLength(embeddedNode.key, key) != embeddedNode.key.len() {
+                            panic!("Key length does not match with the proof one (embeddedNode)");
+                        }
+
+                        key = key.iter().skip(embeddedNode.key.len()).cloned().collect();
+                        if key.len() != 0 {
+                            panic!("Key does not match with the proof one (embeddedNode)");
+                        }
+
+                        // all is fine we return the value
+                        return true;
+                    } else {
+                        wantedHash = childHash;
+                    }
+                },
+                EthTrieNodeType::Leaf | EthTrieNodeType::Extension => {
+                    let val = node.value;
+
+                    // if the relativeKey in the leaf does not math our rest key, we throw!
+                    if Self::matchingNibbleLength(node.key, key) != node.key.len() {
+                        // so we have a wrong leaf here, if we actually expected this node to not exist,
+                        // the last node in this path may be a different leaf or a branch with a empty hash
+                        if key.len() == node.key.len() && i == proof.len() - 1 && header.receipts_root == Option::None {
+                            return val;
+                        }
+
+                        panic!("Key does not match with the proof one (extention|leaf)");
+                    }
+
+                    // remove the items
+                    key = key.iter().skip(node.key.len()).cloned().collect();
+
+                    if key.len() == 0 {
+                        if i != proof.len() - 1 {
+                            panic!("Additional nodes at end of proof (extention|leaf)");
+                        }
+
+                        // if we are expecting a value we need to check
+                        if header.receipts_root && header.receipts_root.compare(val) {
+                            panic!("The proven value was expected to be {:?} but is {:?}", header.receipts_root, val);
+                        }
+
+                        // if we are proven a value which shouldn't exist this must throw an error
+                        if header.receipts_root == Option::None {
+                            panic!("The value shouldn't exist, but is {:?}", val);
+                        }
+
+                        return val;
+                    } else {
+                        // we continue with the hash 
+                        wantedHash = val;
+                    }
+                },
             }
         }
 
-        true
+        // if we expected this to be null and there is not further node since wantedHash is empty or we had a extension as last element, than it is ok not to find leafs
+        if header.receipts_root == Option::None && (lastNode == Option::None || lastNode.node_type == EthTrieNodeType::Extension || wantedHash.len() == 0) {
+            return false
+        }
+
+        // we reached the end of the proof, but not of the path
+        panic!("Unexpected end of proof");
     }
 }
