641a4d69feec224d5146ca9bbfdfe94b4f1dafd3
nearmax <max@nearprotocol.com>
2020-04-30 18:13:49 -0700

    Nit

--- ethrelay/scripts/run_ganache_to_localnet.sh
+++ ethrelay/scripts/run_ganache_to_localnet.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 set -o errexit
 
 # Executes cleanup function at script exit.
-#trap cleanup EXIT
+trap cleanup EXIT
 
 waitport() {
     while ! nc -z localhost $1 ; do sleep 1 ; done
@@ -17,7 +17,7 @@ cleanup() {
     if [ -n "$node_started" ]; then
         docker kill nearcore watchtower > /dev/null &
     fi
-    
+
     # Kill the ganache instance that we started (if we started one and if it's still running).
     if [ -n "$ganache_pid" ] && ps -p $ganache_pid > /dev/null; then
         kill $ganache_pid
@@ -31,7 +31,7 @@ nearnode_running() {
 }
 
 start_nearnode() {
-    echo "ethrelay" | "$DIR/start_localnet.py" --home "$HOME/.near" --image "nearprotocol/nearcore"
+    echo "ethrelay" | "$DIR/start_localnet.py" --home "$DIR/.near" --image "nearprotocol/nearcore:ethdenver"
     waitport $nearnode_port
 }
 
@@ -39,7 +39,7 @@ if nearnode_running; then
     echo "Using existing nearnode instance"
 else
     echo "Starting our own nearnode instance"
-    rm -rf "/Users/maksymzavershynskyi/.near"
+    rm -rf "$DIR/.near"
     start_nearnode
     node_started=1
 fi
@@ -78,17 +78,27 @@ fi
 
 NODE_URL="http://localhost:3030"
 
-echo "Creating account for smart contract:"
-NODE_ENV=local yarn run near --nodeUrl=$NODE_URL --homeDir "$HOME/.near" --keyPath "$HOME/.near/validator_key.json" create_account ethbridge --masterAccount=ethrelay --initialBalance 100000000 || echo "Skip creating ethbridge accout"
-echo "Deploying smart contract:"
-NODE_ENV=local yarn run near --nodeUrl=$NODE_URL --homeDir "$HOME/.near" --keyPath "$HOME/.near/validator_key.json" deploy --contractName ethbridge --wasmFile "$DIR/../../ethbridge/res/eth_bridge.wasm" || echo "Skip deploying ethbridge smart contract"
+# The master account ID to create accounts
+NEAR_MASTER_ACCOUNT_ID="ethrelay"
+# The account ID to push blocks
+NEAR_RELAYER_ACCOUNT_ID="ethrelay-pusher"
+# The account ID used as ETH bridge
+NEAR_ETHBRIDGE_ACCOUNT_ID="ethbridge"
 
-echo "Running relayer"
-#BRIDGE_VALIDATE_ETHASH=false \
-#    NEAR_NODE_URL="http://localhost:3030" \
-#    NEAR_NODE_NETWORK_ID=local \
-#    ETHEREUM_NODE_URL="ws://localhost:$ganache_port" \
-#    node "$DIR/../index.js" &
-#
-## Successfully stop after 5m
-#sleep 300 && kill -0 $$ && echo "Successfully worked for 5m, stopped"
+echo "Creating account to push blocks:"
+NODE_ENV=local yarn run near --nodeUrl=$NODE_URL --homeDir "$DIR/.near" --keyPath "$DIR/.near/validator_key.json" create_account $NEAR_RELAYER_ACCOUNT_ID --masterAccount=$NEAR_MASTER_ACCOUNT_ID --initialBalance 100000000 || echo "Skip creating ethbridge accout"
+echo "Creating account for smart contract:"
+NODE_ENV=local yarn run near --nodeUrl=$NODE_URL --homeDir "$DIR/.near" --keyPath "$DIR/.near/validator_key.json" create_account $NEAR_ETHBRIDGE_ACCOUNT_ID --masterAccount=$NEAR_MASTER_ACCOUNT_ID --initialBalance 100000000 || echo "Skip creating ethbridge accout"
+echo "Deploying smart contract:"
+NODE_ENV=local yarn run near --nodeUrl=$NODE_URL --homeDir "$DIR/.near" --keyPath "$DIR/.near/validator_key.json" deploy --contractName $NEAR_ETHBRIDGE_ACCOUNT_ID --wasmFile "$DIR/../../ethbridge/res/eth_bridge.wasm" || echo "Skip deploying ethbridge smart contract"
+
+BRIDGE_VALIDATE_ETHASH=false \
+    NEAR_NODE_URL="http://localhost:3030" \
+    NEAR_NODE_NETWORK_ID=local \
+    NEAR_RELAYER_ACCOUNT_ID=$NEAR_RELAYER_ACCOUNT_ID \
+    NEAR_ETHBRIDGE_ACCOUNT_ID=$NEAR_ETHBRIDGE_ACCOUNT_ID \
+    ETHEREUM_NODE_URL="ws://localhost:$ganache_port" \
+    node "$DIR/../index.js" &
+
+# Successfully stop after 5m
+sleep 300 && kill -0 $$ && echo "Successfully worked for 5m, stopped"
