0202d81e40c4a06a6cec6776a1dca1309468b0cd
Joy Wang <wangqiaoyi.joy@gmail.com>
2020-05-25 10:29:50 -0700

    calculate merkle proof

--- libs-rs/btc-client/src/lib.rs
+++ libs-rs/btc-client/src/lib.rs
@@ -28,7 +28,14 @@ pub struct BlockHeader {
     nonce: u32
 }
 
-#[derive(Hash, Clone, Eq, PartialEq)]
+// Merkle proof for a single transaction.
+pub struct MerkleProof {
+    tx_hash: HashStr, // transaction hash that the Merkle proof is computed for.
+    tx_index: u32, // index of the transaction among all transactions.
+    siblings: Vec<HashStr> // sibling hashes of txId which comprise the Merkle proof.
+}
+
+#[derive(Default, Hash, Clone, Eq, PartialEq)]
 struct HashStr {
     value: String
 }
@@ -42,10 +49,10 @@ impl BtcClientContract {
         &mut self,
         block_header: BlockHeader,
     ) {
-        if (Self::verify_header(self, block_header.clone())) {
+        if (Self::verify_header(self, &block_header)) {
             println!("Added new block header. Block hash: {}", block_header.block_hash.value);
             self.blocks.insert(block_header.block_hash.clone(), block_header.clone());
-            self.most_recent_block_hash = block_header.block_hash.clone();
+            self.most_recent_block_hash = block_header.block_hash;
         }
     }
 
@@ -55,15 +62,21 @@ impl BtcClientContract {
      */
     pub fn verify_header(
         &self,
-        block_header: BlockHeader
+        block_header: &BlockHeader
     ) -> bool {
-        let calculated_block_hash = Self::calculate_block_hash(block_header.clone());
+        let nbits_hex = Self::get_hex(block_header.n_bits);
+        let calculated_block_hash = Self::calculate_block_hash(&block_header);
+
         if (calculated_block_hash != block_header.block_hash.value) {
             return false;
         }
-
         // TODO verify difficulty.
         if (Self::within6Confirms(self, block_header.clone())) {
+            return false
+        }
+        
+        // Verify the hash is smaller than the target.
+        if (calculated_block_hash > nbits_hex) {
             return false;
         }
         return true;
@@ -88,18 +101,71 @@ impl BtcClientContract {
     /**
      * Accepts proof of a specific transaction hash and verifies that the header in the 
      * proof was verified, and then checks that the merkle path in the proof is correct.
+     *
+     * Returns true if the tx is in the block given by 'txBlockHash' and the block is in Bitcoin's main chain.
+     * Returns false if the tx is exactly 64 bytes long (to guard against a Merkle tree collision) or fails verification.
+     * 
+     * tx_index is the index of the tx within the block
+     * sibling are the merkle siblings of tx
      */
-    pub fn verify_txn(txHash: String) {
+    pub fn verify_tx(
+        &self,
+        merkle_proof: &MerkleProof,
+        block_hash: &String
+    ) -> bool {
+        let block_header = match self.blocks.get(&HashStr{ value: block_hash.to_string() }) {
+            Some(block_header) => block_header,
+            None => return false
+        };
 
+        if (block_header.merkle_root_hash != Self::calculate_merkle_root(merkle_proof)) {
+            return false;
+        }
+
+        // TODO check for 6 confirmations.
+        // TODO check for paid fee.
+        // TODO check tx_hash is in the main chain, ie not a fork.
+
+        return true;
     } 
 
 }
 
 impl BtcClientContract { 
-    fn calculate_block_hash(block_header: BlockHeader) -> String {
+    fn calculate_merkle_root(merkle_proof: &MerkleProof) -> HashStr {
+        let mut result_hash = merkle_proof.tx_hash.clone();
+        let mut index = merkle_proof.tx_index;
+
+        for i in 0..merkle_proof.siblings.len() {
+            let mut left_hash: HashStr = Default::default();
+            let mut right_hash: HashStr = Default::default();
+
+            if index % 2 == 0 {
+                left_hash = result_hash;
+                right_hash = merkle_proof.siblings[i].clone();
+            } else {
+                left_hash = merkle_proof.siblings[i].clone();
+                right_hash = result_hash;
+            }
+
+            result_hash = Self::concat_hash(left_hash.clone(), right_hash.clone());
+            // println!("index: {}\n result: {}\n left: {}\n right: {}\n", i.to_string(), result_hash.value, left_hash.value, right_hash.value);
+            index = index / 2;
+        }
+
+        return result_hash;
+    }
+
+    fn concat_hash(a: HashStr, b: HashStr) -> HashStr {
+        let ab = [Self::little_endian(&a.value), Self::little_endian(&b.value)].concat();
+        return HashStr{ value: Self::double_sha256(&ab) };
+    }
+
+
+    fn calculate_block_hash(block_header: &BlockHeader) -> String {
         let version_hex = Self::get_hex(block_header.version);
-        let prev_hash_hex = Self::little_endian(block_header.prev_block_hash.value);
-        let merkle_root_hex = Self::little_endian(block_header.merkle_root_hash.value);
+        let prev_hash_hex = Self::little_endian(&block_header.prev_block_hash.value);
+        let merkle_root_hex = Self::little_endian(&block_header.merkle_root_hash.value);
         let time_hex = Self::get_hex(block_header.time);
         let nbits_hex = Self::get_hex(block_header.n_bits);
         let nonce_hex = Self::get_hex(block_header.nonce);
@@ -111,20 +177,31 @@ impl BtcClientContract {
         // println!("{}", nbits_hex);
         // println!("{}", nonce_hex);
 
-        let aggregate_hex_str = [version_hex, prev_hash_hex, merkle_root_hex, time_hex, nbits_hex, nonce_hex].concat();
-        let bytes = hex::decode(aggregate_hex_str).unwrap();
-
-        return Self::little_endian(hex::encode(env::sha256(&env::sha256(&bytes))));
+        return Self::double_sha256(&[version_hex, prev_hash_hex, merkle_root_hex, time_hex, nbits_hex, nonce_hex].concat());
     }
 
+    /**
+     * Returns a double sha256 hash of a string in little endian format.
+     */
+    fn double_sha256(str: &String) -> String {
+        let bytes = hex::decode(str).unwrap();
+        return Self::little_endian(&hex::encode(env::sha256(&env::sha256(&bytes))));
+    }
+
+    /**
+     * Returns a hex string of a number.
+     */
     fn get_hex(n: u32) -> String {
         let mut bytes: [u8; 4] = [0; 4];
         LittleEndian::write_u32_into(&[n], &mut bytes);
         return hex::encode(bytes);
     }
 
-    fn little_endian(hex_str: String) -> String {
-        let mut vec = hex::decode(hex_str).unwrap();
+    /**
+     * Returns the little endian version of the given hex string.
+     */
+    fn little_endian(hex_str: &String) -> String {
+        let mut vec = hex::decode(&hex_str).unwrap();
         vec.reverse();
         return hex::encode(&vec);
     }
--- libs-rs/btc-client/src/tests.rs
+++ libs-rs/btc-client/src/tests.rs
@@ -1,4 +1,4 @@
-use crate::{BtcClientContract, BlockHeader, HashStr};
+use crate::{BtcClientContract, BlockHeader, HashStr, MerkleProof};
 use std::collections::HashMap;
 use near_sdk::{env};
 
@@ -42,6 +42,7 @@ mod tests {
             most_recent_block_hash: HashStr { value: "".to_string() }, 
             blocks: HashMap::new() 
         };
+        // Block 0.
         let block_header = BlockHeader{ 
             block_hash: HashStr{ value: "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f".to_string() },
             version: 1,
@@ -52,7 +53,70 @@ mod tests {
             nonce: 2083236893
         };
         contract.accept_header(block_header);
-        // TODO test other verifications.
+    }
+
+    #[test]
+    fn verify_tx() {
+        let context = get_context(vec![], false);
+        testing_env!(context);
+
+        let mut contract = BtcClientContract { 
+            most_recent_block_hash: HashStr { value: "".to_string() }, 
+            blocks: HashMap::new() 
+        };
+        // Block 100000.
+        let block_header = BlockHeader{ 
+            block_hash: HashStr{ value: "000000000003ba27aa200b1cecaad478d2b00432346c3f1f3986da1afd33e506".to_string() },
+            version: 1,
+            prev_block_hash: HashStr{ value:"000000000002d01c1fccc21636b607dfd930d31d01c3a62104612a1719011250".to_string() },
+            merkle_root_hash: HashStr{ value:"f3e94742aca4b5ef85488dc37c06c3282295ffec960994b2c0d5ac2a25a95766".to_string() },
+            time: 1293623863,
+            n_bits: 453281356,
+            nonce: 274148111
+        };
+        contract.accept_header(block_header);
+
+        let merkle_proof = MerkleProof { 
+            tx_hash: HashStr{ value: "fff2525b8931402dd09222c50775608f75787bd2b87e56995a7bdd30f79702c4".to_string() },
+            tx_index: 1,
+            siblings: vec![
+                HashStr{ value: "8c14f0db3df150123e6f3dbbf30f8b955a8249b62ac1d1ff16284aefa3d06d87".to_string() },
+                HashStr{ value: "8e30899078ca1813be036a073bbf80b86cdddde1c96e9e9c99e9e3782df4ae49".to_string() }
+            ]
+        };
+
+        let success = contract.verify_tx(&merkle_proof, &"000000000003ba27aa200b1cecaad478d2b00432346c3f1f3986da1afd33e506".to_string());
+        assert_eq!(true, success);
+    }
+
+    /**
+        Transactions for block 100K:
+        txs: [
+            '8c14f0db3df150123e6f3dbbf30f8b955a8249b62ac1d1ff16284aefa3d06d87',
+            'fff2525b8931402dd09222c50775608f75787bd2b87e56995a7bdd30f79702c4',
+            '6359f0868171b1d194cbee1af2f16ea598ae8fad666d9b012c8ed2b79a236ec4',
+            'e9a66845e05d5abc0ad04ec80f774a7e585c6e8db975962d069a522137b80c1d'
+        ],
+        merkleRoot: 'f3e94742aca4b5ef85488dc37c06c3282295ffec960994b2c0d5ac2a25a95766',
+        hashLeftPair: 'ccdafb73d8dcd0173d5d5c3c9a0770d0b3953db889dab99ef05b1907518cb815',
+        hashRightPair: '8e30899078ca1813be036a073bbf80b86cdddde1c96e9e9c99e9e3782df4ae49'
+     */
+    #[test]
+    fn calculate_merkle_root() {
+        let context = get_context(vec![], false);
+        testing_env!(context);
+
+        let merkle_proof = MerkleProof { 
+            tx_hash: HashStr{ value: "fff2525b8931402dd09222c50775608f75787bd2b87e56995a7bdd30f79702c4".to_string() },
+            tx_index: 1,
+            siblings: vec![
+                HashStr{ value: "8c14f0db3df150123e6f3dbbf30f8b955a8249b62ac1d1ff16284aefa3d06d87".to_string() },
+                HashStr{ value: "8e30899078ca1813be036a073bbf80b86cdddde1c96e9e9c99e9e3782df4ae49".to_string() }
+            ]
+        };
+
+        let merkle_root_hash = BtcClientContract::calculate_merkle_root(&merkle_proof);
+        assert_eq!("f3e94742aca4b5ef85488dc37c06c3282295ffec960994b2c0d5ac2a25a95766", merkle_root_hash.value);
     }
 
     #[test]
@@ -60,6 +124,7 @@ mod tests {
         let context = get_context(vec![], false);
         testing_env!(context);
         
+        // Block 0.
         let block_header = BlockHeader{ 
             block_hash: HashStr{ value: "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f".to_string() },
             version: 1,
@@ -69,7 +134,7 @@ mod tests {
             n_bits: 486604799,
             nonce: 2083236893
         };
-        let block_hash = BtcClientContract::calculate_block_hash(block_header);
+        let block_hash = BtcClientContract::calculate_block_hash(&block_header);
         assert_eq!("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f", block_hash);
     }
 }
