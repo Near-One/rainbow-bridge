bb2483aa371f80ea79f7e387988b056e02266ecd
chefsale <sandi@tenzen.io>
2020-05-29 19:20:54 +0100

    Added rest of the command to the main
    Test Plan:
    ```
    sandi@sandi-ThinkPad-X1-Carbon-7th ~/w/n/r/environment (rainbowup/refactor)> node index.js --help
    Usage: index [options] [command]
    
    Options:
      -V, --version    output the version number
      -h, --help       display help for command
    
    Commands:
      clean
      start <service>
      prepare
      test
      help [command]   display help for command
    sandi@sandi-ThinkPad-X1-Carbon-7th ~/w/n/r/environment (rainbowup/refactor)> node index.js clean
    TODO (chefsale): Implement CleanCommand
    sandi@sandi-ThinkPad-X1-Carbon-7th ~/w/n/r/environment (rainbowup/refactor)> node index.js prepare
    TODO(chefsale): Implement PrepareCommand
    sandi@sandi-ThinkPad-X1-Carbon-7th ~/w/n/r/environment (rainbowup/refactor)> node index.js test
    TODO(chefsale): Implement TestCommand
    sandi@sandi-ThinkPad-X1-Carbon-7th ~/w/n/r/environment (rainbowup/refactor)>
    
    ```

--- environment/index.js
+++ environment/index.js
@@ -1,12 +1,19 @@
 'use strict'
 
-const {StartCommand} = require('./commands/start');
 const {program} = require('commander');
+
+const {CleanCommand} = require('./commands/clean');
+const {PrepareCommand} = require('./commands/prepare');
+const {StartCommand} = require('./commands/start');
+const {TestCommand} = require('./commands/test');
 
 program.version('0.1.0');
 
 async function main() {
+  program.command('clean').action(CleanCommand.execute);
   program.command('start <service>').action(StartCommand.execute);
+  program.command('prepare').action(PrepareCommand.execute);
+  program.command('test').action(TestCommand.execute);
   await program.parseAsync(process.argv);
 }
 
