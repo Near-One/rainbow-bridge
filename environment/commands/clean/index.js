7776768e56d89e6ddaa6f40ab6bdb73b053d5f97
chefsale <sandi@tenzen.io>
2020-05-29 14:10:44 +0100

    Added all the commands to the nodejs implementation

---
+++ environment/commands/clean/index.js
@@ -0,0 +1,7 @@
+class CleanCommand {
+  static async execute() {
+    console.log("TODO (chefsale): Implement CleanCommand");
+  }
+}
+
+exports.CleanCommand = CleanCommand;
--- environment/commands/run/index.js
+++
@@ -1,7 +0,0 @@
-const {RainbowRunner} = require('../../lib/runner');
-
-class RunCommand {
-  async execute(service) { await (new RainbowRunner()).run(service) }
-}
-
-exports.RunCommand = RunCommand;
