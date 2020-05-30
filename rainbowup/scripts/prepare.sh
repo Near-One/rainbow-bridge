a3d6a6c5ddf73c105246ccb998d247fdfa02ebf2
chefsale <sandi@tenzen.io>
2020-05-30 02:05:31 +0100

    Added the prepare step implementation
    Test Plan:
    ```
    node index.js prepare --core-src /home/sandi/workspace/near/core --bridge-src /home/sandi/workspace/near/rainbow-bridge
    Cleaning up Process Manager Daemon.
    prepare
    2020-05-30T12:57:42: PM2 log: Launching in no daemon mode
    2020-05-30T12:57:42: PM2 log: App [prepare:0] starting in -fork mode-
    2020-05-30T12:57:42: PM2 log: App [prepare:0] online
    2020-05-30T12:57:42: PM2 log: Unable to spawn a proccess.
        Blocking waiting for file lock on build directory
    Terminated
    Compiled source of nearcore
        Finished release [optimized] target(s) in 0.19s
        Finished release [optimized] target(s) in 0.21s
    Compiled Rust contracts
    ~/workspace/near/rainbow-bridge/libs-sol/emitter ~/workspace/near/rainbow-bridge/libs-sol
    yarn install v1.22.4
    [1/4] Resolving packages...
    success Already up-to-date.
    Done in 1.61s.
    yarn run v1.22.4
    $ /home/sandi/workspace/near/rainbow-bridge/libs-sol/emitter/node_modules/.bin/oz compile
    - Nothing to compile, all contracts are up to date.
    Done in 2.64s.
    ~/workspace/near/rainbow-bridge/libs-sol
    ~/workspace/near/rainbow-bridge/libs-sol/example-token-locker ~/workspace/near/rainbow-bridge/libs-sol
    yarn install v1.22.4
    warning package-lock.json found. Your project contains lock files generated by tools other than Yarn. It is advised not to mix package managers in order to avoid resolution inconsistencies caused by unsynchronized lock files. To clear this warning, remove package-lock.json.
    [1/4] Resolving packages...
    success Already up-to-date.
    Done in 0.60s.
    yarn run v1.22.4
    $ /home/sandi/workspace/near/rainbow-bridge/libs-sol/example-token-locker/node_modules/.bin/oz compile
    - Nothing to compile, all contracts are up to date.
    Done in 2.31s.
    ~/workspace/near/rainbow-bridge/libs-sol
    Built Solidity contracts
    yarn install v1.22.4
    [1/4] Resolving packages...
    success Already up-to-date.
    Done in 0.79s.
    Compiled ethashproof module
    2020-05-30T12:59:34: PM2 log: App [prepare:0] exited with code [0] via signal [SIGINT]
    2020-05-30T12:59:34: PM2 log: App [prepare:0] starting in -fork mode-
    2020-05-30T12:59:34: PM2 log: App [prepare:0] online
       Compiling openssl-sys v0.9.56
       Compiling librocksdb-sys v6.7.4
       Compiling near-runtime-configs v0.1.0 (/home/sandi/workspace/near/core/core/runtime-configs)
       Compiling near-pool v0.1.0 (/home/sandi/workspace/near/core/chain/pool)
       Compiling wasmer-singlepass-backend v0.17.0
       Compiling near-chain-configs v0.1.0 (/home/sandi/workspace/near/core/core/chain-configs)
    ...
    ...
    ```

--- rainbowup/scripts/prepare.sh
+++ rainbowup/scripts/prepare.sh
@@ -1,34 +1,70 @@
 #!/bin/bash
+set -euo pipefail
 
-CORE_SRC="~/.rainbowup/core"
-BRIDGE_SRC="~/.rainbowup/bridge"
-
-while : ; do
-  case "$1" in 
-    --nearcore_source)
-       [ -n "${CORE_SRC}" ] && usage
-       $CORE_SRC="$2"
-       shift 2 ;;
+BRIDGE_SRC=""
+ORE_SRC=""
+ARGUMENT_LIST=(
+    "source"
+    "nearcore_source"
+)
+# read arguments
+opts=$(getopt \
+    --longoptions "$(printf "%s:," "${ARGUMENT_LIST[@]}")" \
+    --name "$(basename "$0")" \
+    --options "" \
+    -- "$@"
+)
+eval set --$opts
+while [[ $# -gt 0 ]] ; do
+  case "$1" in
     --source)
-       [ -n "${BRIDGE_SRC}" ] && usage
-       BRIDGE_SRC="$2"
-       shift 2 ;;
+      BRIDGE_SRC=$2
+      shift 2
+      ;;
+    --nearcore_source)
+      CORE_SRC=$2
+      shift 2
+      ;;
     *)
-       break ;;
+      break
+      ;;
   esac
 done
 
-cd $CORE_SRC && cargo build --package neard --bin neard
+
+
+if test -z "$CORE_SRC"
+then
+echo "near-core repo not specified..."
+CORE_SRC=~/.rainbowup/core
+git clone "https://github.com/nearprotocol/nearcore" $CORE_SRC
+fi
+
+if test -z "$BRIDGE_SRC"
+then
+echo "rainbow-bridge repo not specified..."
+BRIDGE_SRC=~/.rainbowup/bridge
+git clone "https://github.com/near/rainbow-bridge/" $BRIDGE_SRC
+cd $BRIDGE_SRC
+git submodule update --init --recursive
+fi
+
+cd $CORE_SRC
+cargo build --package neard --bin neard
 echo "Compiled source of nearcore"
 
-cd $BRIDGE_SRC/libs-rs && ./build_all.sh
+cd $BRIDGE_SRC/libs-rs
+./build_all.sh
 echo "Compiled Rust contracts"
 
-cd $BRIDGE_SRC/libs-sol && ./build_all.sh
+cd $BRIDGE_SRC/libs-sol
+./build_all.sh
 echo "Built Solidity contracts"
 
-cd $BRIDGE_SRC/environment && yarn
+# Install environment dependencies
+cd $BRIDGE_SRC/environment
+yarn
 
-cd $BRIDGE_SRC/environment/vendor/ethashproof && ./build.sh
+cd $BRIDGE_SRC/environment/vendor/ethashproof
+./build.sh
 echo 'Compiled ethashproof module'
-
