690ee14d588d3271ea7dbba1986590cc5c66fb4c
nearmax <max@nearprotocol.com>
2020-05-31 20:02:34 -0700

    Add init-eth-test-contracts (complete), init-near-test-contracts (partially complete), and transfer-fun-eth2near (not complete) commands

--- environment/index.js
+++ environment/index.js
@@ -6,6 +6,9 @@ const {CleanCommand} = require('./commands/clean');
 const {PrepareCommand} = require('./commands/prepare');
 const {StartCommand} = require('./commands/start');
 const {TestCommand} = require('./commands/test');
+const {TransferFunETH2NEAR} = require('./commands/transfer-fun-eth2near');
+const {InitETHTestContracts} = require('./commands/init-eth-test-contracts');
+const {InitNEARTestContracts} = require('./commands/init-near-test-contracts');
 
 program.version('0.1.0');
 
@@ -17,5 +20,35 @@ program.command('prepare')
             '')
     .option('--core-src <core_src>', 'Path to the nearcore source', '');
 program.command('test').action(TestCommand.execute);
+program.command('transfer-fun-eth2near')
+    .action(TransferFunETH2NEAR.execute)
+    .option('--eth-node-url <eth_node_url>', 'The URL of the Ethereum node.', '')
+    .option('--eth-token-address <eth_token_address>', 'Address of the ERC20 token on Ethereum network that will be transferred.', '')
+    .option('--eth-locker-address <eth_locker_address>', 'Address of the token locker contract on Ethereum network that will be locking the token on Ethereum side.', '')
+    .option('--eth-locker-abi-path <eth_locker_abi_path>', 'Path to an ABI file describing Ethereum locker contract interface', '')
+    .option('--eth-sender-sk <eth_sender_sk>', 'Secret key of the account that owns the token.', '')
+    .option('--near-node-url <near_node_url>', 'The URL of the NEAR node.', '')
+    .option('--near-network-id <near_network_id>', 'The identifier of the NEAR network that the given NEAR node is expected to represent.', '')
+    .option('--near-locker-address <near_locker_address>', 'Address of the token locker contract on NEAR network that will be unlocking the token on Near side.', '')
+    .option('--near-token-address <near_token_address>', 'Address of the fungible token on NEAR network that will be transferred.', '')
+    .option('--near-receiver-account <near_receiver_account>', 'Address of the account that will be receiving the token on NEAR side. This account will also be paying for the gas.', '')
+    .option('--near-receiver-sk <near_receier_sk>', 'Secret key of the account on NEAR that will be receiving the token. This key will be used to pay for the gas.', '')
+    .option('--amount <amount>', 'Amount of tokens to transfer.', '') ;
+program.command('init-eth-test-contracts')
+    .action(InitETHTestContracts.execute)
+    .option('--eth-node-url <eth_node_url>', 'The URL of the Ethereum node.', '')
+    .option('--eth-master-sk <eth_master_sk>', 'The secret key of the master account on Ethereum that can be used to deploy and initialize the test contracts.' +
+        ' This account will also own the initial supply of the ERC20 tokens.', '')
+    .option('--contracts-dir <contracts_dir>', 'The path to the abi and bin files of the test contracts.', '');
+program.command('init-near-test-contracts')
+    .action(InitNEARTestContracts.execute)
+    .option('--near-node-url <near_node_url>', 'The URL of the NEAR node.', '')
+    .option('--near-network-id <near_network_id>', 'The identifier of the NEAR network that the given NEAR node is expected to represent.', '')
+    .option('--contracts-dir <contracts_dir>', 'The path to the wasm files of the test contracts.', '')
+    .option('--near-master-account <near_master_account>', 'The account of the master account on NEAR that can be used to deploy and initialize the test contracts.' +
+        ' This account will also own the initial supply of the fungible tokens.', '')
+    .option('--near-master-sk <near_master_sk>', 'The secret key of the master account.', '')
+    .option('--validate_ethash <validate_ethash>', '', 'true')
+    .option('--near-prover-account <near_prover_account>', 'The account of the prover contract that it locker contract can use to validate proofs.', '');
 
 program.parse(process.argv);
