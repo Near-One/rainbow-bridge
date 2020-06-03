455177bb393d5e142cbdd73442d9cd1733650ba9
Bo Yao <bo@nearprotocol.com>
2020-06-03 10:59:25 -0700

    optional start endblock

--- environment/commands/dump-eth-headers/index.js
+++ environment/commands/dump-eth-headers/index.js
@@ -5,6 +5,13 @@ const fs = require('fs').promises;
 class DumpETHHeaders {
   static async execute({path, startBlock, endBlock, ethNodeUrl}) {
     let web3 = new Web3(ethNodeUrl);
+    if (!endBlock) {
+      endBlock = await web3.eth.getBlockNumber();
+    }
+    if (!startBlock) {
+      startBlock = Math.max(0, endBlock - 43000);
+    }
+
     for (let b = startBlock; b <= endBlock; b++) {
       console.log(`Downloading block ${b}`);
       let block = await web3.eth.getBlock(b);
