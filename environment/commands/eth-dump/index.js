55050a2c5800174678e306a6a0c8fa9bca0dc031
Bo Yao <bo@nearprotocol.com>
2020-06-09 15:10:26 -0700

    fix existing eth-proof test

---
+++ environment/commands/eth-dump/index.js
@@ -0,0 +1,112 @@
+const Web3 = require('web3');
+const Path = require('path');
+const fs = require('fs').promises;
+const { web3BlockToRlp, execute } = require('../../lib/eth-relay');
+const {
+  EthProofExtractor,
+  receiptFromWeb3,
+  logFromWeb3,
+} = require('../../lib/eth-proof-extractor');
+const utils = require('ethereumjs-util');
+
+class ETHDump {
+  static async execute(kindOfData, { path, startBlock, endBlock, ethNodeUrl }) {
+    const web3 = new Web3(ethNodeUrl);
+    const extractor = new EthProofExtractor();
+    extractor.initialize(ethNodeUrl);
+
+    if (kindOfData != 'headers' && kindOfData != 'proofs') {
+      console.log("Usage: node index.js dump headers\n       node index.js dump proofs");
+      process.exit(2);
+    }
+    if (!endBlock) {
+      endBlock = await web3.eth.getBlockNumber();
+    } else {
+      endBlock = Number(endBlock);
+    }
+    if (!startBlock) {
+      startBlock = Math.max(0, Number(endBlock) - 43000 + 1);
+    } else if (Number(startBlock) < 0) {
+      startBlock = Math.max(0, Number(endBlock) + Number(startBlock) + 1)
+    } else {
+      startBlock = Number(startBlock);
+    }
+    console.log(`Downloading block ${endBlock} down to ${startBlock} to ${path}. ${endBlock - startBlock + 1} blocks in total.`)
+
+    for (let b = endBlock; b >= startBlock; b--) {
+      if (kindOfData == 'headers') {
+        await ETHDump.dumpHeaders(web3, b, path);
+      } else if (kindOfData == 'proofs') {
+        await ETHDump.dumpProofs(web3, extractor, b, path);
+      }
+    }
+
+    try {
+      // Only WebSocket provider can close.
+      web3.currentProvider.connection.close();
+    } catch (e) {
+    }
+    extractor.destroy();
+  }
+
+  static async dumpHeaders(web3, b, path) {
+    console.log(`Downloading block ${b}`);
+    const blockRlp = web3.utils.bytesToHex(web3BlockToRlp(await web3.eth.getBlock(b)));
+    console.log(`Processing block ${b}`);
+
+    const unparsedBlock = await execute(`./vendor/ethashproof/cmd/relayer/relayer ${blockRlp} | sed -e '1,/Json output/d'`);
+    const block = JSON.parse(unparsedBlock);
+    await ETHDump.saveBlock(b, block, path);
+  }
+
+  static async dumpProofs(web3, extractor, b, path) {
+    const block = await web3.eth.getBlock(b);
+    for (const txHash of block.transactions) {
+      const receipt = await extractor.extractReceipt(txHash);
+      if (receipt.logs.length == 0) {
+        continue;
+      }
+      const block = await extractor.extractBlock(receipt.blockNumber);
+      const tree = await extractor.buildTrie(block);
+      const proof = await extractor.extractProof(block, tree, receipt.transactionIndex);
+      let log_index = -1;
+      for (const log of receipt.logs) {
+        log_index++;
+        console.log('===========================================================================');
+        console.log(`BLOCK NUMBER ${receipt.blockNumber}`);
+        console.log(`TX_HASH ${txHash}`);
+        console.log(`LOG_INDEX ${log_index}`);
+
+        const log_entry_data = logFromWeb3(log).serialize();
+        const receipt_index = proof.txIndex;
+        const receipt_data = receiptFromWeb3(receipt).serialize();
+        const header_data = proof.header.serialize();
+        const _proof = [];
+        for (const node of proof.receiptProof) {
+          _proof.push(utils.rlp.encode(node));
+        }
+
+        const skip_bridge_call = false;
+
+        const args = {
+          log_index: log_index,
+          log_entry_data: log_entry_data,
+          receipt_index: receipt_index,
+          receipt_data: receipt_data,
+          header_data: header_data,
+          proof: _proof,
+          skip_bridge_call: skip_bridge_call,
+        };
+
+        console.log(args);
+      }
+    }
+  }
+
+  static async saveBlock(i, block, path) {
+    let file = Path.join(path, `${i}.json`);
+    await fs.writeFile(file, JSON.stringify(block));
+  }
+}
+
+exports.ETHDump = ETHDump;
