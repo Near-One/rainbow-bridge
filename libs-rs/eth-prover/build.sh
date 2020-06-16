f7882fe52e01e1ba036c39481d4af621cb9f9d86
Bo Yao <bo@nearprotocol.com>
2020-06-16 14:31:21 -0700

    make build.sh and build_all.sh work from all dir

--- libs-rs/eth-prover/build.sh
+++ libs-rs/eth-prover/build.sh
@@ -3,5 +3,7 @@
 # Exit script as soon as a command fails.
 set -e
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 RUSTFLAGS='-C link-arg=-s' cargo +stable build --target wasm32-unknown-unknown --release
-cp ../target/wasm32-unknown-unknown/release/eth_prover.wasm ../res/
+cp $DIR/../target/wasm32-unknown-unknown/release/eth_prover.wasm $DIR/../res/
