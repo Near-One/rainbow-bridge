48c6b5a69db14eb45a9fefab3dbcfde57436032c
Bo Yao <bo@nearprotocol.com>
2020-07-02 17:52:45 -0700

    dump verify near proofs

--- libs-sol/nearprover/test/NearProver.js
+++ libs-sol/nearprover/test/NearProver.js
@@ -1,4 +1,6 @@
 const { borshifyOutcomeProof } = require(`../../../environment/lib/borsh`);
+const bs58 = require('bs58');
+const fs = require('fs').promises;
 
 const NearProver = artifacts.require('NearProver');
 const NearBridgeMock = artifacts.require('NearBridgeMock');
@@ -10,10 +12,6 @@ contract('NearProver', function ([_, addr1]) {
     });
 
     it('should be ok', async function () {
-        await this.bridge.setBlockMerkleRoot(526, '0x727e29b62b5c27d50811916798e32e0464c21eb4bbb81a59eb02c37d65ea668f');
-        const proof1 = borshifyOutcomeProof(require('./proof1.json'));
-        expect(await this.prover.proveOutcome(proof1, 526)).to.be.true;
-
         await this.bridge.setBlockMerkleRoot(498, '0x22f00dd154366d758cd3e4fe81c1caed8e0db6227fe4b2b52a8e5a468aa0a723');
         const proof2 = borshifyOutcomeProof(require('./proof2.json'));
         expect(await this.prover.proveOutcome(proof2, 498)).to.be.true;
@@ -22,9 +20,24 @@ contract('NearProver', function ([_, addr1]) {
         const proof3 = borshifyOutcomeProof(require('./proof3.json'));
         expect(await this.prover.proveOutcome(proof3, 1705)).to.be.true;
 
-        // TODO: Investigate weird block path returned by the node.
-        // await this.bridge.setBlockMerkleRoot(5563, '0x82415d76338be5e5a45524042595f1d9e95f1836c59921bc3fab3201a1519581');
-        // const proof4 = borshifyOutcomeProof(require('./proof4.json'));
-        // expect(await this.prover.proveOutcome(proof4, 5563)).to.be.true;
+        await this.bridge.setBlockMerkleRoot(5563, '0x82415d76338be5e5a45524042595f1d9e95f1836c59921bc3fab3201a1519581');
+        const proof4 = borshifyOutcomeProof(require('./proof4.json'));
+        expect(await this.prover.proveOutcome(proof4, 5563)).to.be.true;
     });
+
+    if (process.env['NEAR_PROOFS_DIR']) {
+        it('should able to verify proofs from dump', async function () {
+            let proofFiles = await fs.readdir(process.env['NEAR_PROOFS_DIR']);
+            
+            for (let i = 1; i < proofFiles.length; i++) {
+                let proof = require(process.env['NEAR_PROOFS_DIR'] +'/' + proofFiles[i]);
+                let height = proof.block_header_lite.inner_lite.height;
+                await this.bridge.setBlockMerkleRoot(height, '0x'+bs58.decode(proof.block_header_lite.inner_lite.block_merkle_root).toString('hex'));
+                proof = borshifyOutcomeProof(proof);
+                expect(await this.prover.proveOutcome(proof, height)).to.be.true;
+                console.log('proved proof ' + proofFiles[i]);
+            }
+        }) 
+    }
+
 });
