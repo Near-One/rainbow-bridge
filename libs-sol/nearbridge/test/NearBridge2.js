b3b2aa8d3f90dda67ebc31d709af2c9a7d4f0a47
Maksym Zavershynskyi <max@near.org>
2020-07-14 07:37:45 -0700

    Nit

--- libs-sol/nearbridge/test/NearBridge2.js
+++ libs-sol/nearbridge/test/NearBridge2.js
@@ -17,36 +17,30 @@ function borshify (block) {
         bs58.decode(block.prev_block_hash),
         bs58.decode(block.next_block_inner_hash),
         Buffer.concat([
-            // @ts-ignore
-            Web3.utils.toBN(block.inner_lite.height).toBuffer('le', 8),
+            web3.utils.toBN(block.inner_lite.height).toBuffer('le', 8),
             bs58.decode(block.inner_lite.epoch_id),
             bs58.decode(block.inner_lite.next_epoch_id),
             bs58.decode(block.inner_lite.prev_state_root),
             bs58.decode(block.inner_lite.outcome_root),
-            // @ts-ignore
-            Web3.utils.toBN(block.inner_lite.timestamp).toBuffer('le', 8),
+            web3.utils.toBN(block.inner_lite.timestamp).toBuffer('le', 8),
             bs58.decode(block.inner_lite.next_bp_hash),
             bs58.decode(block.inner_lite.block_merkle_root),
         ]),
         bs58.decode(block.inner_rest_hash),
 
         Buffer.from([1]),
-        // @ts-ignore
-        Web3.utils.toBN(block.next_bps.length).toBuffer('le', 4),
+        web3.utils.toBN(block.next_bps.length).toBuffer('le', 4),
         Buffer.concat(
             block.next_bps.map(nextBp => Buffer.concat([
-                // @ts-ignore
-                Web3.utils.toBN(nextBp.account_id.length).toBuffer('le', 4),
+                web3.utils.toBN(nextBp.account_id.length).toBuffer('le', 4),
                 Buffer.from(nextBp.account_id),
                 nextBp.public_key.substr(0, 8) === 'ed25519:' ? Buffer.from([0]) : Buffer.from([1]),
                 bs58.decode(nextBp.public_key.substr(8)),
-                // @ts-ignore
-                Web3.utils.toBN(nextBp.stake).toBuffer('le', 16),
+                web3.utils.toBN(nextBp.stake).toBuffer('le', 16),
             ])),
         ),
 
-        // @ts-ignore
-        Web3.utils.toBN(block.approvals_after_next.length).toBuffer('le', 4),
+        web3.utils.toBN(block.approvals_after_next.length).toBuffer('le', 4),
         Buffer.concat(
             block.approvals_after_next.map(
                 signature => signature === null
