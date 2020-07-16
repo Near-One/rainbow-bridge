f5e7015b83a389e9f795bb4247b5a07b49c55496
Bo Yao <bo@nearprotocol.com>
2020-07-16 12:48:30 -0700

    add a test to repro #191

--- libs-sol/nearbridge/test/NearBridge2.js
+++ libs-sol/nearbridge/test/NearBridge2.js
@@ -6,13 +6,13 @@ const Ed25519 = artifacts.require('Ed25519');
 const NearBridge = artifacts.require('NearBridge');
 const NearDecoder = artifacts.require('NearDecoder');
 
-async function timeIncreaseTo (seconds) {
+async function timeIncreaseTo(seconds) {
     const delay = 1000 - new Date().getMilliseconds();
     await new Promise(resolve => setTimeout(resolve, delay));
     await time.increaseTo(seconds);
 }
 
-function borshify (block) {
+function borshify(block) {
     return Buffer.concat([
         bs58.decode(block.prev_block_hash),
         bs58.decode(block.next_block_inner_hash),
@@ -128,3 +128,33 @@ contract('NearBridge2', function ([_, addr1]) {
 //     });
 // });
 
+contract('Test adding blocks in new epoch when bps change', function ([_, addr1]) {
+    beforeEach(async function () {
+
+    });
+
+    it('should be ok', async function () {
+        this.decoder = await NearDecoder.new();
+        this.bridge = await NearBridge.new((await Ed25519.deployed()).address, web3.utils.toBN(1e18), web3.utils.toBN(3600));
+        await this.bridge.deposit({ value: web3.utils.toWei('1') });
+
+        const block244 = require('./244.json');
+        const block304 = require('./304.json');
+        const block308 = require('./308.json');
+        const block368 = require('./368.json');
+
+        await this.bridge.initWithBlock(borshify(block244));
+        await this.bridge.blockHashes(244);
+
+        let now = await time.latest();
+        await timeIncreaseTo(now.add(time.duration.seconds(3600)));
+
+        await this.bridge.addLightClientBlock(borshify(block304));
+        await this.bridge.blockHashes(304);
+
+        now = await time.latest();
+        await timeIncreaseTo(now.add(time.duration.seconds(3600)));
+        await this.bridge.addLightClientBlock(borshify(block368));
+        await this.bridge.blockHashes(368);
+    });
+});
