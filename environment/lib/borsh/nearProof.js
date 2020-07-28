f0ef16cada70fbc2e81394012b50d187432038c2
Bo Yao <bo@nearprotocol.com>
2020-07-28 15:22:27 -0700

    fix invalid proof

--- environment/lib/borsh/nearProof.js
+++ environment/lib/borsh/nearProof.js
@@ -1,7 +1,7 @@
 const Web3 = require('web3');
 const bs58 = require('bs58');
 
-function borshifyOutcomeProof (proof) {
+function borshifyOutcomeProof(proof) {
     const statusToBuffer = status => {
         console.log(status.SuccessValue);
         if ('SuccessValue' in status) {
@@ -61,7 +61,7 @@ function borshifyOutcomeProof (proof) {
             bs58.decode(proof.block_header_lite.inner_lite.next_epoch_id),
             bs58.decode(proof.block_header_lite.inner_lite.prev_state_root),
             bs58.decode(proof.block_header_lite.inner_lite.outcome_root),
-            Web3.utils.toBN(proof.block_header_lite.inner_lite.timestamp).toBuffer('le', 8),
+            Web3.utils.toBN(proof.block_header_lite.inner_lite.timestamp_nanosec).toBuffer('le', 8),
             bs58.decode(proof.block_header_lite.inner_lite.next_bp_hash),
             bs58.decode(proof.block_header_lite.inner_lite.block_merkle_root),
 
