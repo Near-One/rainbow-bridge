3e63ce1b8776d2de3f8b2b8307262b008c62fade
Bo Yao <bo@nearprotocol.com>
2020-07-28 11:03:54 -0700

    remove timeout for near prover verify dumped proof test

--- libs-sol/nearprover/test/NearProver.js
+++ libs-sol/nearprover/test/NearProver.js
@@ -27,17 +27,18 @@ contract('NearProver', function ([_, addr1]) {
 
     if (process.env['NEAR_PROOFS_DIR']) {
         it('should able to verify proofs from dump', async function () {
+            this.timeout(0);
             let proofFiles = await fs.readdir(process.env['NEAR_PROOFS_DIR']);
-            
+
             for (let i = 1; i < proofFiles.length; i++) {
-                let proof = require(process.env['NEAR_PROOFS_DIR'] +'/' + proofFiles[i]);
+                let proof = require(process.env['NEAR_PROOFS_DIR'] + '/' + proofFiles[i]);
                 let height = proof.block_header_lite.inner_lite.height;
-                await this.bridge.setBlockMerkleRoot(height, '0x'+bs58.decode(proof.block_header_lite.inner_lite.block_merkle_root).toString('hex'));
+                await this.bridge.setBlockMerkleRoot(height, '0x' + bs58.decode(proof.block_header_lite.inner_lite.block_merkle_root).toString('hex'));
                 proof = borshifyOutcomeProof(proof);
                 expect(await this.prover.proveOutcome(proof, height)).to.be.true;
                 console.log('proved proof ' + proofFiles[i]);
             }
-        }) 
+        })
     }
 
 });
