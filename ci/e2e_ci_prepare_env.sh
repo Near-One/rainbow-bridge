7c5d679008819dfb7617f4caea9b89b84c236015
Bo Yao <bo@nearprotocol.com>
2020-08-04 16:56:16 -0700

    fix env

--- ci/e2e_ci_prepare_env.sh
+++ ci/e2e_ci_prepare_env.sh
@@ -9,23 +9,4 @@ fi
 
 export GOROOT=~/.go
 export GOPATH=~/go
-export PATH=$$GOPATH/bin:$$GOROOT/bin:$$PATH
-
-# hard link pm2 logs to current dir, so buildkite can pick up them as artifacts
-mkdir -p ~/.rainbow/logs/eth-relay
-mkdir -p ~/.rainbow/logs/near-relay
-mkdir -p ~/.rainbow/logs/ganache
-touch eth-relay-out.log
-touch eth-relay-err.log
-touch near-relay-out.log
-touch near-relay-err.log
-touch ganache-out.log
-touch ganache-err.log
-if [[ ! -f ~/.rainbow/logs/eth-relay/out.log ]]; then
-    ln eth-relay-out.log ~/.rainbow/logs/eth-relay/out.log
-    ln eth-relay-err.log ~/.rainbow/logs/eth-relay/err.log
-    ln near-relay-out.log ~/.rainbow/logs/near-relay/out.log
-    ln near-relay-err.log ~/.rainbow/logs/near-relay/err.log
-    ln ganache-out.log ~/.rainbow/logs/ganache/out.log
-    ln ganache-err.log ~/.rainbow/logs/ganache/err.log
-fi
+export PATH=$GOPATH/bin:$GOROOT/bin:$$PATH
