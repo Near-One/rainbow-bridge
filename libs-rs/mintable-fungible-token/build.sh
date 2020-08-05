785ea965e507fd4e639a44c854b61328b8532ea2
Bo Yao <bo@nearprotocol.com>
2020-08-04 17:36:06 -0700

    workaround buildkite

--- libs-rs/mintable-fungible-token/build.sh
+++ libs-rs/mintable-fungible-token/build.sh
@@ -3,10 +3,15 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+if [[ -z "$BUILDKITE" ]]; then
+     userflag="-u $UID:$UID"
+else
+     userflag=""
+fi
+
 docker run \
      --mount type=bind,source=$DIR/..,target=/host \
-     --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
-     -u $UID:$UID \
+     --cap-add=SYS_PTRACE --security-opt seccomp=unconfined $userflag \
      -w /host/mintable-fungible-token \
      -e RUSTFLAGS='-C link-arg=-s' \
      nearprotocol/contract-builder \
