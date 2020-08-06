d0338020fce1bee4671258d661312a766293394e
Bo Yao <bo@nearprotocol.com>
2020-08-06 09:43:30 -0700

    touch

--- ci/e2e_ci_prepare_log.sh
+++ ci/e2e_ci_prepare_log.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 # hard link pm2 logs to current dir, so buildkite can pick up them as artifacts
 
+mkdir -p ~/.rainbow/logs/eth-relay
+mkdir -p ~/.rainbow/logs/near-relay
+mkdir -p ~/.rainbow/logs/ganache
+mkdir -p ~/.rainbow/logs/near-watchdog
+touch eth-relay-out.log
+touch eth-relay-err.log
+touch near-relay-out.log
+touch near-relay-err.log
+touch ganache-out.log
+touch ganache-err.log
+touch near-watchdog-out.log
+touch near-watchdog-err.log
 if [[ ! -f ~/.rainbow/logs/eth-relay/out.log ]]; then
-    mkdir -p ~/.rainbow/logs/eth-relay
-    mkdir -p ~/.rainbow/logs/near-relay
-    mkdir -p ~/.rainbow/logs/ganache
-    touch eth-relay-out.log
-    touch eth-relay-err.log
-    touch near-relay-out.log
-    touch near-relay-err.log
-    touch ganache-out.log
-    touch ganache-err.log
-    touch near-watchdog-out.log
-    touch near-watchdog-err.log
     ln eth-relay-out.log ~/.rainbow/logs/eth-relay/out.log
     ln eth-relay-err.log ~/.rainbow/logs/eth-relay/err.log
     ln near-relay-out.log ~/.rainbow/logs/near-relay/out.log
