6f472cdba0bb48f12a5038c5eb5be8425c22c023
Bo Yao <bo@nearprotocol.com>
2020-08-06 18:11:23 -0700

    fix collect log on ci

--- ci/e2e_ci_prepare_log.sh
+++ ci/e2e_ci_prepare_log.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # hard link pm2 logs to current dir, so buildkite can pick up them as artifacts
 
+rm -rf ~/.rainbow/logs/*
 mkdir -p ~/.rainbow/logs/eth-relay
 mkdir -p ~/.rainbow/logs/near-relay
 mkdir -p ~/.rainbow/logs/ganache
@@ -13,11 +14,11 @@ touch ganache-out.log
 touch ganache-err.log
 touch near-watchdog-out.log
 touch near-watchdog-err.log
-ln eth-relay-out.log ~/.rainbow/logs/eth-relay/out.log || true
-ln eth-relay-err.log ~/.rainbow/logs/eth-relay/err.log || true
-ln near-relay-out.log ~/.rainbow/logs/near-relay/out.log || true
-ln near-relay-err.log ~/.rainbow/logs/near-relay/err.log || true
-ln ganache-out.log ~/.rainbow/logs/ganache/out.log || true
-ln ganache-err.log ~/.rainbow/logs/ganache/err.log || true
-ln near-watchdog-out.log ~/.rainbow/logs/near-watchdog/out.log || true
-ln near-watchdog-err.log ~/.rainbow/logs/near-watchdog/err.log || true
+ln eth-relay-out.log ~/.rainbow/logs/eth-relay/out.log
+ln eth-relay-err.log ~/.rainbow/logs/eth-relay/err.log
+ln near-relay-out.log ~/.rainbow/logs/near-relay/out.log
+ln near-relay-err.log ~/.rainbow/logs/near-relay/err.log
+ln ganache-out.log ~/.rainbow/logs/ganache/out.log
+ln ganache-err.log ~/.rainbow/logs/ganache/err.log
+ln near-watchdog-out.log ~/.rainbow/logs/near-watchdog/out.log
+ln near-watchdog-err.log ~/.rainbow/logs/near-watchdog/err.log
