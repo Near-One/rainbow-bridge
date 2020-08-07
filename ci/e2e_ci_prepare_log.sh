a04b9cad4f1c5168bebf2f1f037bef1bbbaf6d63
Bo Yao <bo@nearprotocol.com>
2020-08-07 15:13:57 -0700

    fix log, ln didn't on amazonlinux runner

--- ci/e2e_ci_prepare_log.sh
+++ ci/e2e_ci_prepare_log.sh
@@ -1,24 +1,5 @@
-#!/bin/bash
-# hard link pm2 logs to current dir, so buildkite can pick up them as artifacts
-
-rm -rf ~/.rainbow/logs/*
-mkdir -p ~/.rainbow/logs/eth-relay
-mkdir -p ~/.rainbow/logs/near-relay
-mkdir -p ~/.rainbow/logs/ganache
-mkdir -p ~/.rainbow/logs/near-watchdog
-touch eth-relay-out.log
-touch eth-relay-err.log
-touch near-relay-out.log
-touch near-relay-err.log
-touch ganache-out.log
-touch ganache-err.log
-touch near-watchdog-out.log
-touch near-watchdog-err.log
-ln eth-relay-out.log ~/.rainbow/logs/eth-relay/out.log
-ln eth-relay-err.log ~/.rainbow/logs/eth-relay/err.log
-ln near-relay-out.log ~/.rainbow/logs/near-relay/out.log
-ln near-relay-err.log ~/.rainbow/logs/near-relay/err.log
-ln ganache-out.log ~/.rainbow/logs/ganache/out.log
-ln ganache-err.log ~/.rainbow/logs/ganache/err.log
-ln near-watchdog-out.log ~/.rainbow/logs/near-watchdog/out.log
-ln near-watchdog-err.log ~/.rainbow/logs/near-watchdog/err.log
+function finish {
+    cp -r ~/.rainbow/logs .
+}
+trap finish ERR
+trap finish EXIT
