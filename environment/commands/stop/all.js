a37b9ed3eab0b2afdce24f3b167e0e6a5b7b242d
Alex Kouprin <kpr@nearprotocol.com>
2020-08-07 12:52:17 +0300

    using pm2-promise - WIP

--- environment/commands/stop/all.js
+++ environment/commands/stop/all.js
@@ -1,28 +1,26 @@
+const ProcessManager = require('pm2-promise');
+
 const { StopLocalNearNodeCommand } = require('./near.js');
-const { StopManagedProcessCommand } = require('./process.js');
 
-// TODO please do it in a better way
-
-class A {
-}
-
-stop = function(name) {
-    try {
-        var a = A;
-        a._name = name;
-        StopManagedProcessCommand.execute(a);
-    } catch (err) {
-        console.log('Error stopping', name, err);
-    }
+function stop(serviceName) {
+    ProcessManager.delete(serviceName).then(() => {
+        console.log(serviceName, 'successfully stopped...');
+    }).catch((err) => {
+        if (!err.message.includes('process or namespace not found')) {
+            console.log(serviceName, 'error stopping the process due to:', err);
+        } else {
+            console.log(serviceName, 'already stopped');
+        }
+    });
 }
 
 class StopAllCommands {
-    static execute() {
+    static async execute() {
         console.log('Stopping all processes...');
 
         StopLocalNearNodeCommand.execute();
-        for (const name of ['ganache', 'eth-relay', 'near-relay', 'near-watchdog']) {
-            stop(name);
+        for (const serviceName of ['ganache', 'eth-relay', 'near-relay', 'near-watchdog']) {
+            await stop(serviceName);
         }
 
         console.log('Stopping all processes done');
