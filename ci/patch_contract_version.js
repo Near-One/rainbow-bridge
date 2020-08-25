df0e7b8e100b3fca78aa697f0c909994f3faeb38
Bo Yao <bo@nearprotocol.com>
2020-08-25 16:06:20 -0700

    fix typo

--- ci/patch_contract_version.js
+++ ci/patch_contract_version.js
@@ -6,18 +6,22 @@ const fs = require('fs')
 async function main() {
   let packageJson = require(path.join(__dirname, '../environment/package.json'))
   if (process.env.PATCH_RAINBOW_BRIDGE_SOL) {
-    packageJson[
+    packageJson.dependencies[
       'rainbow-bridge-sol'
     ] = `near/rainbow-bridge-sol#${process.env.PATCH_RAINBOW_BRIDGE_SOL}`
   }
   if (process.env.PATCH_RAINBOW_BRIDGE_RS) {
-    packageJson[
+    packageJson.dependencies[
       'rainbow-bridge-rs'
     ] = `near/rainbow-bridge-rs#${process.env.PATCH_RAINBOW_BRIDGE_RS}`
   }
   console.log('Contract versions:')
-  console.log(`rainbow-bridge-sol: ${packageJson['rainbow-bridge-sol']}`)
-  console.log(`rainbow-bridge-rs: ${packageJson['rainbow-bridge-rs']}`)
+  console.log(
+    `rainbow-bridge-sol: ${packageJson.dependencies['rainbow-bridge-sol']}`
+  )
+  console.log(
+    `rainbow-bridge-rs: ${packageJson.dependencies['rainbow-bridge-rs']}`
+  )
   if (
     !process.env.PATCH_RAINBOW_BRIDGE_SOL &&
     !process.env.PATCH_RAINBOW_BRIDGE_RS
