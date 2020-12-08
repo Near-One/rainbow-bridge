8e845365a83d3e5c14f5a2a113311b9ca12a51a9
Alex Kouprin <kpr@nearprotocol.com>
2020-12-08 19:58:20 +0000

    feat(adapter): near-to-eth methods + minor upds

--- testing/index.js
+++ testing/index.js
@@ -5,19 +5,21 @@ const {
 } = require('./transfer-eth-erc20')
 const {
   mintErc20,
-  getEthErc20Balance,
-  getEthAddressBySecretKey,
+  getErc20Balance,
+  getClientBlockHeightHash,
+  getAddressBySecretKey,
   ethToNearApprove,
   ethToNearLock,
-  ethToNearFindProof
+  nearToEthUnlock
 } = require('./adapter')
 
 exports.TransferETHERC20ToNear = TransferETHERC20ToNear
 exports.TransferEthERC20FromNear = TransferEthERC20FromNear
 exports.DeployToken = DeployToken
 exports.mintErc20 = mintErc20
-exports.getEthErc20Balance = getEthErc20Balance
-exports.getEthAddressBySecretKey = getEthAddressBySecretKey
+exports.getErc20Balance = getErc20Balance
+exports.getClientBlockHeightHash = getClientBlockHeightHash
+exports.getAddressBySecretKey = getAddressBySecretKey
 exports.ethToNearApprove = ethToNearApprove
 exports.ethToNearLock = ethToNearLock
-exports.ethToNearFindProof = ethToNearFindProof
+exports.nearToEthUnlock = nearToEthUnlock
--- utils/borshify-proof.js
+++ utils/borshify-proof.js
@@ -3,7 +3,6 @@ const bs58 = require('bs58')
 
 function borshifyOutcomeProof (proof) {
   const statusToBuffer = (status) => {
-    console.log(status)
     if ('SuccessValue' in status) {
       const data = Buffer.from(status.SuccessValue, 'base64')
       return Buffer.concat([
