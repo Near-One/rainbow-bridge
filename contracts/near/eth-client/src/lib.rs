80fb8ad01e74c47e054956db7862f07363e7d379
Septen <kirill@aurora.dev>
2021-01-09 12:54:48 +0000

    BSC client: minor refactoring and fixes.

--- contracts/near/eth-client/src/lib.rs
+++ contracts/near/eth-client/src/lib.rs
@@ -20,6 +20,19 @@ near_sdk::setup_alloc!();
 #[cfg(test)]
 mod tests;
 
+#[cfg(feature = "bsc")]
+mod bsc_constants {
+    pub const BSC_EPOCH_SIZE: usize = 200;
+    pub const BSC_EXTRA_VANITY: usize = 32;
+    pub const BSC_EXTRA_SEAL: usize = 65;
+    pub const BSC_VALIDATOR_BYTES_SIZE: usize = 20;
+    pub const BSC_DIFFICULTY_IN_TURN: usize = 2;
+    pub const BSC_DIFFICULTY_NO_TURN: usize = 1;
+}
+
+#[cfg(feature = "bsc")]
+use bsc_constants::*;
+
 #[derive(Default, Debug, Clone, BorshDeserialize, BorshSerialize)]
 #[cfg_attr(not(target_arch = "wasm32"), derive(Serialize, Deserialize))]
 pub struct DoubleNodeWithMerkleProof {
@@ -184,8 +197,9 @@ impl EthClient {
         let mut epoch_header: H256 = H256([0; 32].into());
 
         // check if the current mode is bsc POSA, then store the epoch header.
+        #[cfg(feature = "bsc")]
         if validate_header_mode == String::from("bsc") {
-            if !EthClient::is_epoch(header.number) {
+            if !EthClient::bsc_is_epoch(header.number) {
                 panic!("The initial header for POSA have to be an epoch header");
             }
             epoch_header = header.hash.unwrap();
@@ -322,9 +336,11 @@ impl EthClient {
             panic!("Header is too old to have a chance to appear on the canonical chain.");
         }
 
-        if EthClient::is_epoch(header.number) && self.validate_header_mode == String::from("bsc") {
+        #[cfg(feature = "bsc")]
+        if EthClient::bsc_is_epoch(header.number) && self.validate_header_mode == String::from("bsc") {
             self.epoch_header = header.hash.unwrap();
         }
+
         let parent_info = self
             .infos
             .get(&header.parent_hash)
@@ -480,16 +496,15 @@ impl EthClient {
             return false;
         }
 
-        let (extra_vanity, extra_seal, validator_bytes_length) = (32, 65, 20);
-        let is_epoch = EthClient::is_epoch(header.number);
-        let signers_bytes = header.extra_data.len() - (extra_vanity + extra_seal);
+        let is_epoch = EthClient::bsc_is_epoch(header.number);
+        let signers_bytes = header.extra_data.len() - (BSC_EXTRA_VANITY + BSC_EXTRA_SEAL);
         // check it is not an epoch header but contains the signers.
         if !is_epoch && signers_bytes != 0 {
             return false;
         }
 
         // check if it is an epoch header and contains the signers.
-        if is_epoch && signers_bytes % validator_bytes_length != 0 {
+        if is_epoch && signers_bytes % BSC_VALIDATOR_BYTES_SIZE != 0 {
             return false;
         }
 
@@ -526,18 +541,18 @@ impl EthClient {
     }
 
     // check if the block is an epoch header.
-    fn is_epoch(number: u64) -> bool {
-        number % 200 == 0
+    #[cfg(feature = "bsc")]
+    fn bsc_is_epoch(number: u64) -> bool {
+        number % BSC_EPOCH_SIZE as u64 == 0
     }
 
     // check if the author is the signer.
     #[cfg(feature = "bsc")]
     fn bsc_is_author(&self, header: &BlockHeader) -> bool {
-        let extra_seal = 65;
         let seal_hash = self.bsc_seal_hash(header, U256(self.chain_id.into()));
 
         // get the signature from header extra_data
-        let signature = header.extra_data[header.extra_data.len() - extra_seal..].to_vec();
+        let signature = header.extra_data[header.extra_data.len() - BSC_EXTRA_SEAL..].to_vec();
         let mut sig = [0u8; 65];
         sig.copy_from_slice(&signature[..]);
 
@@ -575,8 +590,6 @@ impl EthClient {
     // check if the author address is valid and is in the validator set.
     #[cfg(feature = "bsc")]
     fn bsc_is_validator(&self, header: &BlockHeader) -> bool {
-        let (extra_vanity, extra_seal, address_size) = (32, 65, 20);
-
         let epoch_header = self.bsc_get_epoch_header();
 
         if !self.bsc_is_in_validator_set(&epoch_header, header.author) {
@@ -587,22 +600,28 @@ impl EthClient {
         if self.validate_header {
             // Ensure that the difficulty corresponds to the turn-ness of the signer
             let validators = epoch_header.extra_data
-                [extra_vanity..(epoch_header.extra_data.len() - extra_seal)]
+                [BSC_EXTRA_VANITY..(epoch_header.extra_data.len() - BSC_EXTRA_SEAL)]
                 .to_vec();
 
             // Get validator offset position.
-            let offset = (header.number % ((validators.len() / address_size) as u64)) as usize;
+            let offset =
+                (header.number % ((validators.len() / BSC_VALIDATOR_BYTES_SIZE) as u64)) as usize;
 
             // validate the current author if it's turn with the difficulty.
-            let (diff_in_turn, diff_no_turn) = (2, 1);
-            let in_turn =
-                Address::from(&validators[(offset * address_size)..((offset + 1) * address_size)]);
+            let expected_validator_in_turn = Address::from(
+                &validators[(offset * BSC_VALIDATOR_BYTES_SIZE)
+                    ..((offset + 1) * BSC_VALIDATOR_BYTES_SIZE)],
+            );
 
-            if in_turn == header.author && header.difficulty != U256(diff_in_turn.into()) {
+            if expected_validator_in_turn == header.author
+                && header.difficulty != U256(BSC_DIFFICULTY_IN_TURN.into())
+            {
                 return false;
             }
 
-            if !(in_turn == header.author) && header.difficulty != U256(diff_no_turn.into()) {
+            if !(expected_validator_in_turn == header.author)
+                && header.difficulty != U256(BSC_DIFFICULTY_NO_TURN.into())
+            {
                 return false;
             }
         }
@@ -683,13 +702,13 @@ impl EthClient {
     // check if the author is in the validators set.
     #[cfg(feature = "bsc")]
     fn bsc_is_in_validator_set(&self, epoch_header: &BlockHeader, add: Address) -> bool {
-        let (extra_vanity, extra_seal, address_size) = (32, 65, 20);
         let validators = epoch_header.extra_data
-            [extra_vanity..(epoch_header.extra_data.len() - extra_seal)]
+            [BSC_EXTRA_VANITY..(epoch_header.extra_data.len() - BSC_EXTRA_SEAL)]
             .to_vec();
 
-        for x in 0..(validators.len() / address_size) {
-            let value = &validators[(x * address_size)..((x + 1) * address_size)];
+        for x in 0..(validators.len() / BSC_VALIDATOR_BYTES_SIZE) {
+            let value =
+                &validators[(x * BSC_VALIDATOR_BYTES_SIZE)..((x + 1) * BSC_VALIDATOR_BYTES_SIZE)];
             let _add: Address = Address::from(value);
             if _add == add {
                 return true;
