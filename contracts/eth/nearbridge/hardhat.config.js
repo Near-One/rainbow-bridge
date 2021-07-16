a50deb2e0565633928a5f7a1e5f4a85dc2f0fe03
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-07-16 14:10:37 +0200

    feat: storage layout

--- contracts/eth/nearbridge/hardhat.config.js
+++ contracts/eth/nearbridge/hardhat.config.js
@@ -1,18 +1,25 @@
 require('@nomiclabs/hardhat-ethers');
 require('@nomiclabs/hardhat-waffle');
 require('solidity-coverage');
+require('./tasks/storage');
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+    newStorageLayoutPath: './storageLayout',
     solidity: {
         version: '0.8.3',
         settings: {
             optimizer: {
                 enabled: true,
                 runs: 1000
-            }
+            },
+            outputSelection: {
+                '*': {
+                    '*': ['storageLayout'],
+                },
+            },
         }
     }
 };
