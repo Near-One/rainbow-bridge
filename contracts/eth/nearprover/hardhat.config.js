2c4973c909d1bf72e8ffeb74434027beb733a17c
Septen <kirill@aurora.dev>
2021-12-02 20:18:39 +0200

    NearProver:add scripts&tests for bridge upgradability

--- contracts/eth/nearprover/hardhat.config.js
+++ contracts/eth/nearprover/hardhat.config.js
@@ -1,6 +1,28 @@
 require('@nomiclabs/hardhat-ethers');
 require('solidity-coverage');
 
+require('dotenv').config();
+
+const ETH_PRIVATE_KEY = process.env.ETH_PRIVATE_KEY;
+const INFURA_API_KEY = process.env.INFURA_API_KEY || '';
+
+task('upgrade-provers-bridge-address-to', 'Upgrades the provided prover to use the bridge at the provided address')
+    .addParam('prover', 'Prover address')
+    .addParam('newBridge', 'The address of the new bridge')
+    .setAction(async taskArgs => {
+        const { upgradeProversBridgeAddressTo } = require('./utils/upgrade_bridge_address.js');
+        await upgradeProversBridgeAddressTo(hre.ethers.provider, taskArgs.prover, taskArgs.newBridge);
+    });
+
+task('get-provers-bridge-address', 'Returns the current bridge address used in the prover at the provided address')
+    .addParam('prover', 'Prover address')
+    .setAction(async taskArgs => {
+        const { getProversBridgeAddress } = require('./utils/upgrade_bridge_address.js');
+        const bridgeAddress = await getProversBridgeAddress(taskArgs.prover);
+        console.log(`Prover address: ${taskArgs.prover}`);
+        console.log(`Current bridge address: ${bridgeAddress}`);
+    });
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -13,5 +35,23 @@ module.exports = {
                 runs: 1000
             }
         }
+    },
+    networks: {
+        ropsten: {
+            url: `https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
+            accounts: [`0x${ETH_PRIVATE_KEY}`],
+            gasPrice: 50000000000,
+            gasMultiplier: 2,
+        },
+        mainnet: {
+            url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
+            accounts: [`0x${ETH_PRIVATE_KEY}`],
+        },
+        goerli: {
+            url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+            accounts: [`0x${ETH_PRIVATE_KEY}`],
+            gasPrice: 50000000000,
+            gasMultiplier: 2,
+        },
     }
 };
---
+++ contracts/eth/nearprover/utils/upgrade_bridge_address.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+
+const { ethers } = require('hardhat');
+const { assert, expect } = require('chai');
+
+const BRIDGE_ADDRESS_SLOT = 2;
+
+async function upgradeProversBridgeAddressTo(provider, proverAddress, newBridgeAddress) {
+    const nearProverFactory = await ethers.getContractFactory('NearProver');
+    const nearProver = nearProverFactory.attach(proverAddress);
+
+    console.log(`Got prover at address: ${proverAddress}`);
+
+    const initialBridgeAddress = await nearProver.bridge();
+    console.log(`Initial bridge address: ${initialBridgeAddress}`);
+    console.log(`Trying to upgrade bridge address to: ${newBridgeAddress}`);
+
+    const adminWallet = new ethers.Wallet(process.env.ETH_PRIVATE_KEY, provider);
+
+    assert.equal(
+        adminWallet.address,
+        await nearProver.admin(),
+        "The used account is not an admin of NearProver"
+    );
+
+    // Mask matches only on the latest 20 bytes (to store the address)
+    const mask = ethers.BigNumber.from("0x000000000000000000000000ffffffffffffffffffffffffffffffffffffffff");
+    const response = await nearProver
+        .connect(adminWallet)
+        .adminSstoreWithMask(BRIDGE_ADDRESS_SLOT, newBridgeAddress, mask);
+    await response.wait().then(function(receipt) {
+        console.log(`Transaction mined: `);
+        console.log(receipt);
+    });
+
+    const bridgeAddressAfterUpgrade = await nearProver.bridge();
+    console.log(`Bridge address after upgrade: ${bridgeAddressAfterUpgrade}`);
+    expect(bridgeAddressAfterUpgrade.toLowerCase())
+        .to
+        .equal(newBridgeAddress.toLowerCase());
+}
+
+async function getProversBridgeAddress(proverAddress) {
+    const nearProverFactory = await ethers.getContractFactory('NearProver');
+    const nearProver = nearProverFactory.attach(proverAddress);
+    const bridgeAddress = await nearProver.bridge();
+
+    return bridgeAddress;
+}
+
+exports.upgradeProversBridgeAddressTo = upgradeProversBridgeAddressTo;
+exports.getProversBridgeAddress = getProversBridgeAddress;
+
