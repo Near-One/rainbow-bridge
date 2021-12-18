ed5efd3fdba6ae94c72614cd62ab6620a0c7788d
Septen <kirill@aurora.dev>
2021-12-18 20:34:04 +0000

    NearBridge: add tests. Tests refactoring.
    * NearBridge: add tests for admin upgradability.
    * NearBridge: tests refactoring.

--- contracts/eth/nearbridge/test/NearBridge3.js
+++ contracts/eth/nearbridge/test/NearBridge3.js
@@ -1,27 +1,77 @@
 const { ethers } = require('hardhat');
 const { expect } = require('chai');
 
-const { borshify, borshifyInitialValidators } = require('rainbow-bridge-lib/rainbow/borsh')
+const { borshify, borshifyInitialValidators } = require('rainbow-bridge-utils')
 
-let Ed25519, NearBridge;
 
-beforeEach(async function () {
-    Ed25519 = await (await ethers.getContractFactory('Ed25519')).deploy();
-    NearBridge = await (await ethers.getContractFactory('NearBridge')).deploy(
-        Ed25519.address,
-        ethers.BigNumber.from("1000000000000000000"), // 1e18
-        ethers.BigNumber.from("10"), // lock duration
-        ethers.BigNumber.from("20000000000"), // replace duration
-        await (await ethers.getSigners())[0].getAddress(),
-        0
-    );
-    await NearBridge.deposit({ value: ethers.utils.parseEther('1') });
+describe('New tests', () => {
+    let Ed25519;
+    let NearBridge;
+    let AdminWallet;
+
+    beforeEach(async function () {
+        Ed25519 = await (await ethers.getContractFactory('Ed25519')).deploy();
+        [AdminWallet] = await ethers.getSigners();
+        NearBridge = await (await ethers.getContractFactory('NearBridge')).deploy(
+            Ed25519.address,
+            ethers.BigNumber.from("1000000000000000000"), // 1e18
+            ethers.BigNumber.from("10"), // lock duration
+            ethers.BigNumber.from("20000000000"), // replace duration
+            await AdminWallet.getAddress(),
+            0
+        );
+        await NearBridge.deposit({ value: ethers.utils.parseEther('1') });
+    });
+
+    it('Verify init with validators V1', async function () {
+        const initialValidators = borshifyInitialValidators(require('./block_validators_v1_testnet.json').next_bps);
+
+        expect(await NearBridge.initWithValidators(borshifyInitialValidators(require('./block_validators_v1_testnet.json').next_bps)));
+    });
 });
 
-it('Verify init with validators V1', async function () {
-    const initialValidators = borshifyInitialValidators(require('./block_validators_v1_testnet.json').next_bps);
-    console.log(`Initial validators: ${initialValidators.toString('hex')}`);
 
-    expect(await NearBridge.initWithValidators(borshifyInitialValidators(require('./block_validators_v1_testnet.json').next_bps)));
+describe('NearBridge with admin access', () => {
+    const ADMIN_ADDRESS_SLOT = 0;
+    let nearBridge;
+    let adminAccount;
+
+    beforeEach(async () => {
+        ed25519 = await (await ethers.getContractFactory('Ed25519')).deploy();
+        [deployerAccount] = await ethers.getSigners();
+
+        // Make the deployer admin
+        adminAccount = deployerAccount;
+
+        nearBridge = await (await ethers.getContractFactory('NearBridge')).deploy(
+            ed25519.address,
+            ethers.BigNumber.from("1000000000000000000"), // 1e18
+            ethers.BigNumber.from("10"), // lock duration
+            ethers.BigNumber.from("20000000000"), // replace duration
+            await adminAccount.getAddress(),
+            0
+        );
+    });
+
+    describe('Upgradability', async () => {
+        it('should upgrade the admin address from the provided hex string', async () => {
+            const initialAdminAddress = await nearBridge.admin();
+            expect(initialAdminAddress)
+                .to
+                .equal(await adminAccount.address);
+
+            const newAdminAddress = '0x0123456789abcdefcafedeadbeefbea77a1de456';
+            expect(newAdminAddress)
+                .not
+                .equal(initialAdminAddress);
+
+            // Mask matches only on the latest 20 bytes (to store the address)
+            const mask = ethers.BigNumber.from("0x000000000000000000000000ffffffffffffffffffffffffffffffffffffffff");
+            nearBridge.adminSstoreWithMask(ADMIN_ADDRESS_SLOT, newAdminAddress, mask);
+
+            expect((await nearBridge.admin()).toLowerCase())
+                .to
+                .equal(newAdminAddress);
+        });
+    });
 });
-
