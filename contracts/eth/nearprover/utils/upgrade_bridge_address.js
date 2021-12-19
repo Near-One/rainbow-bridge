a657de6eede49429dcfc3ea61e38eb4a5aa8d411
Karim <karim@aurora.de>
2021-12-19 21:06:56 +0200

    Upgrade bridge address script improvments. * Upgrade bridge address script: replace ethers signer with web3

--- contracts/eth/nearprover/utils/upgrade_bridge_address.js
+++ contracts/eth/nearprover/utils/upgrade_bridge_address.js
@@ -1,12 +1,29 @@
+const Web3 = require('web3');
+const createLedgerSubprovider = require('@ledgerhq/web3-subprovider').default;
+const hid = require('@ledgerhq/hw-transport-node-hid').default;
+const ProviderEngine = require('web3-provider-engine');
+const RpcSubprovider = require('web3-provider-engine/subproviders/rpc');
+
 require('dotenv').config();
 
 const { ethers } = require('hardhat');
-const { assert, expect } = require('chai');
-const { EthLedgerSigner } = require('./eth-ledger-signer');
-
+const { assert } = require('chai');
 const BRIDGE_ADDRESS_SLOT = 2;
 
 async function upgradeProversBridgeAddressTo (provider, proverAddress, newBridgeAddress, ledgerKeyPath) {
+    const engine = new ProviderEngine();
+    const getTransport = () => hid.create();
+    const ledger = createLedgerSubprovider(getTransport, {
+        accountsLength: 5,
+        paths: [ledgerKeyPath],
+    });
+
+    engine.addProvider(ledger);
+    const rpcUrl = `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`;
+    console.log(rpcUrl);
+    engine.addProvider(new RpcSubprovider({ rpcUrl }));
+    engine.start();
+    const web3 = new Web3(engine);
     const nearProverFactory = await ethers.getContractFactory('NearProver');
     const nearProver = nearProverFactory.attach(proverAddress);
 
@@ -15,38 +32,28 @@ async function upgradeProversBridgeAddressTo (provider, proverAddress, newBridge
     const initialBridgeAddress = await nearProver.bridge();
     console.log(`Initial bridge address: ${initialBridgeAddress}`);
     console.log(`Trying to upgrade bridge address to: ${newBridgeAddress}`);
-
-    let signer;
-    // We use non-strict unequality as it also includes undefined, 0, etc
-    if (ledgerKeyPath != null) {
-        signer = new EthLedgerSigner(provider, ledgerKeyPath);
-    } else {
-        signer = new ethers.Wallet(process.env.ETH_PRIVATE_KEY, provider);
-    }
+    const abi = require('../artifacts/contracts/NearProver.sol/NearProver.json').abi;
+    const accounts = await web3.eth.getAccounts();
+    const signerAccount = accounts[0];
+    console.log(signerAccount);
+    console.log(`Used account: ${signerAccount}`);
+    const web3Contract = new web3.eth.Contract(abi, proverAddress);
 
     assert.equal(
-        await signer.getAddress(),
+        await signerAccount,
         await nearProver.admin(),
         'The used account is not an admin of NearProver',
     );
 
     // Mask matches only on the latest 20 bytes (to store the address)
-    const mask = ethers.BigNumber.from('0x000000000000000000000000ffffffffffffffffffffffffffffffffffffffff');
-    const response = await nearProver
-        .connect(signer)
-        .adminSstoreWithMask(BRIDGE_ADDRESS_SLOT, newBridgeAddress, mask);
+    const mask = '0x000000000000000000000000ffffffffffffffffffffffffffffffffffffffff';
+    const options = {
+        from: signerAccount,
+        gas: 50000,
+        gasPrice: 150000000000, // 150 Gwei
+    };
+    const response = await web3Contract.methods.adminSstoreWithMask(BRIDGE_ADDRESS_SLOT, newBridgeAddress, mask).send(options);
     console.log(response);
-    console.log('Waiting for tx confirmation...');
-    await response.wait(10).then(function (receipt) {
-        console.log('Transaction mined: ');
-        console.log(receipt);
-    });
-
-    const bridgeAddressAfterUpgrade = await nearProver.bridge();
-    console.log(`Bridge address after upgrade: ${bridgeAddressAfterUpgrade}`);
-    expect(bridgeAddressAfterUpgrade.toLowerCase())
-        .to
-        .equal(newBridgeAddress.toLowerCase());
 }
 
 async function getProversBridgeAddress (proverAddress) {
