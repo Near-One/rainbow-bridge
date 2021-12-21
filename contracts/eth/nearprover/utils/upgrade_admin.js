aaca1ea9a057b92cce7331f25250eb55b7b83b52
Karim <karim@aurora.dev>
2021-12-21 17:13:56 +0200

    Add extra logs

--- contracts/eth/nearprover/utils/upgrade_admin.js
+++ contracts/eth/nearprover/utils/upgrade_admin.js
@@ -1,11 +1,13 @@
 const { ethers } = require('hardhat');
 const { assert, expect } = require('chai');
 const { EthLedgerSigner } = require('./eth-ledger-signer');
+const yesno = require('yesno');
 
 async function getSlotsData (provider, contractAddress, numOfSlotsToDisplay) {
+    console.log(`Contract's ${contractAddress} slots:`);
     for (let i = 0; i < numOfSlotsToDisplay; ++i) {
         const slotData = await provider.getStorageAt(contractAddress, i);
-        console.log(slotData);
+        console.log(`Slot ${i}: ${slotData}`);
     }
 }
 
@@ -17,9 +19,9 @@ async function upgradeAdminAddressTo ({
     adminAddressSlot,
     ledgerKeyPath,
 }) {
-    const currentAdmin = ethers.BigNumber.from(await provider.getStorageAt(contractAddress, Number(adminAddressSlot))).toHexString();
+    const currentAdminAtSlot = ethers.BigNumber.from(await provider.getStorageAt(contractAddress, Number(adminAddressSlot))).toHexString();
     assert.equal(
-        currentAdmin,
+        currentAdminAtSlot,
         currentAdminAddress,
         `The current admin doesn't match at the slot ${adminAddressSlot} contract ${contractAddress}`,
     );
@@ -27,6 +29,8 @@ async function upgradeAdminAddressTo ({
     const adminControlledFactory = await ethers.getContractFactory('AdminControlled');
     const adminControlled = adminControlledFactory.attach(contractAddress);
 
+    await getSlotsData(provider, contractAddress, 5);
+    console.log(`Selected slot: ${adminAddressSlot}`);
     console.log(`Current admin: ${await adminControlled.admin()}`);
     console.log(`Trying to upgrade admin address to: ${newAdminAddress}`);
 
@@ -43,6 +47,15 @@ async function upgradeAdminAddressTo ({
         await adminControlled.admin(),
         `The used account is not an admin of contract ${contractAddress}`,
     );
+
+    const ok = await yesno({
+        question: 'WARRING! this change can\'t be reverted, are you sure that you want to change the admin address?',
+    });
+
+    if (!ok) {
+        console.log('The task was aborted');
+        return;
+    }
 
     // Mask matches only on the latest 20 bytes (to store the address)
     const mask = ethers.BigNumber.from('0x000000000000000000000000ffffffffffffffffffffffffffffffffffffffff');
