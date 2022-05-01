46c5b177756cfe94d9972eeb5f334a90a8d72a97
vldmkr <vladmakaryan@gmail.com>
2022-05-01 05:23:18 +0300

    NEAR2Eth-Relay: Resolve PR review comments (aurora-is-near/bounties#1)

--- near2eth/near2eth-block-relay/index.js
+++ near2eth/near2eth-block-relay/index.js
@@ -190,7 +190,7 @@ class Near2EthRelay {
     const errorDelay = Number(near2ethRelayErrorDelay)
     const afterSubmitDelayMs = Number(near2ethRelayAfterSubmitDelayMs)
 
-    const selectDuration = web3.utils.toBN(Number(near2ethRelayBlockSelectDuration) * 1000_000_000)
+    const selectDurationNs = web3.utils.toBN(Number(near2ethRelayBlockSelectDuration) * 1000_000_000)
 
     ethGasMultiplier = Number(ethGasMultiplier)
     ethUseEip1559 = ethUseEip1559 === 'true'
@@ -209,7 +209,7 @@ class Near2EthRelay {
         }
         this.borshBlock = borshBlock
         this.height = lightClientBlock.inner_lite.height
-        console.log(`The new optimal block is found. Height: ${this.height}. Size: ${this.borshBlock.length} bytes`)
+        console.log(`The new optimal block is found at height ${this.height}, ${this.borshBlock.length} bytes`)
       },
       clean: function () {
         this.startedAt = 0
@@ -220,9 +220,10 @@ class Near2EthRelay {
         return !this.borshBlock
       },
       isSuitable: function ({ borshBlock, lightClientBlock }) {
-        return !this.isEmpty() &&
-               this.height !== lightClientBlock.inner_lite.height &&
-               this.borshBlock.length >= borshBlock.length
+        return this.isEmpty()
+               || (!this.isEmpty()
+                 && this.height !== lightClientBlock.inner_lite.height
+                 && this.borshBlock.length >= borshBlock.length)
       }
     }
     const getGasOptions = async (useEip1559, gasMultiplier) => {
@@ -270,29 +271,22 @@ class Near2EthRelay {
               borshBlock: borshify(lastBlock)
             }
 
-            // Describe the selection logic here so as not to complicate the reading of the code
-            if (nextBlockSelection.isEmpty()) {
-              console.log('The selection of the optimal block has begun.')
-              nextBlockSelection.set(blockCouple)
-            } else if (nextBlockSelection.isSuitable(blockCouple)) {
+            if (nextBlockSelection.isSuitable(blockCouple)) {
               nextBlockSelection.set(blockCouple)
             }
 
-            const selectDelay = selectDuration
+            const selectDelayNs = selectDurationNs
               .add(web3.utils.toBN(nextBlockSelection.startedAt))
               .sub(web3.utils.toBN(lastBlock.inner_lite.timestamp))
-            if (selectDelay.cmpn(0) > 0) {
-              const selectDelaySeconds = selectDelay.div(web3.utils.toBN(1000_000_000))
-              console.log(`Last light client block: ${lastBlock.inner_lite.height}`)
-              console.log(`Time left to make a decision: ${selectDelaySeconds.toString()} seconds`)
+            if (selectDelayNs.cmpn(0) > 0) {
+              const selectDelaySeconds = selectDelayNs.div(web3.utils.toBN(1000_000_000))
+              console.log(`Last block height ${lastBlock.inner_lite.height}, ${blockCouple.borshBlock.length} bytes, ${selectDelaySeconds.toString()}s left`)
               await sleep(1000) // Block creation time is approximately one second, is an additional cli argument needed here?
               continue
-            } else {
-              console.log('Time to make a decision is over!')
             }
 
             console.log(
-              `Trying to submit new block at height ${nextBlockSelection.height}`
+              `Trying to submit new block at height ${nextBlockSelection.height}, ${nextBlockSelection.borshBlock.length} bytes`
             )
 
             // Check whether master account has enough balance at stake.
