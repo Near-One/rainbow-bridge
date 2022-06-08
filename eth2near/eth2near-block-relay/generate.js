912035c6648ba4198d3a7c6b96bbf87b87dc357b
Kirill <kirill@aurora.dev>
2022-06-08 09:42:39 +0000

    Update more tests for `eip1559`. Update res.

---
+++ eth2near/eth2near-block-relay/generate.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const {
+    Eth2NearRelay
+} = require('./index')
+
+// Add your web3 endpoint url here
+const WEB3_ENDPOINT_URL = 'YOUR_ENDPOINT_HERE';
+
+async function main() {
+    var args = process.argv.slice(2);
+    const blockNumber = parseInt(args[0], 10);
+    const relay = new Eth2NearRelay()
+    relay.initialize('', { ethNodeUrl: WEB3_ENDPOINT_URL, gasPerTransaction: '1', totalSubmitBlock: 0, gasPerTransaction: '1', metricsPort: 0, nearNetworkId: 'mainnet' })
+    const res = await relay.getParseBlock(blockNumber)
+    fs.writeFileSync(`${__dirname}/../../contracts/near/eth-client/src/data/${blockNumber}.json`, JSON.stringify(res));
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
