299b8c9a12da501418f8d786847f4c5360a5cbcf
Olga Kunyavsksya <olga.kunyavskaya@aurora.dev>
2022-07-14 16:07:39 +0300

    flag arg for eth_node_url

--- eth2near/eth2near-block-relay-rs/src/main.rs
+++ eth2near/eth2near-block-relay-rs/src/main.rs
@@ -1,9 +1,12 @@
 use clap::Parser;
+use std::string::String;
 
 #[derive(Parser,Default,Debug)]
 #[clap(version, about="Eth2 to Near Relay")]
 struct Arguments {
-    eth_node_url: std::string::String,
+    #[clap(long, default_value_t = String::from("https://lodestar-kiln.chainsafe.io"))]
+    /// endpoint to full node of Eth2 Beacon chain with Light Client API
+    eth_node_url: String,
 }
 
 fn main() {
