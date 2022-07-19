6ee7e9cda0b60a9eabb83a321f3eeae31e18fcc3
Olga Kunyavsksya <olga.kunyavskaya@aurora.dev>
2022-07-19 13:13:10 +0300

    get blockHeader

--- eth2near/eth2near-block-relay-rs/src/eth1_rpc_client.rs
+++ eth2near/eth2near-block-relay-rs/src/eth1_rpc_client.rs
@@ -1,5 +1,9 @@
+use std::collections::HashMap;
 use std::error::Error;
+use std::fmt::format;
+use eth_types::BlockHeader;
 use reqwest::blocking::Client;
+use serde_json::Value;
 
 pub struct Eth1RPCClient {
     endpoint_url: String,
@@ -16,13 +20,46 @@ impl Eth1RPCClient {
 
     pub fn get_block_header_by_number(
         &self,
-        number: &str,
-    ) -> Result<(), Box<dyn Error>> {
-        Ok(())
-    }
+        number: u64,
+    ) -> Result<BlockHeader, Box<dyn Error>> {
+        let mut map = HashMap::new();
+        map.insert("id", 0);
 
-    fn get_block_by_number(&self, number: &str) -> Result<(), Box<dyn Error>>  {
-        Ok(())
+        let mut json_str = format!("{}\"id\": 0,\
+        \"jsonrpc\": \"2.0\",\
+        \"method\": \"eth_getBlockByNumber\",\
+        \"params\": [\"0x{:x}\",false]\
+        {}", "{", number, "}");
+        println!("json_str: {}", json_str);
+
+        let mut value: Value = serde_json::from_str(&json_str).unwrap();
+        println!("value: {:?}", value);
+
+        let res = self.client.post(&self.endpoint_url).json(&value).send()?.text().unwrap();
+        println!("res: {:?}", res);
+
+        let mut val: Value = serde_json::from_str(&res).unwrap();
+        let mut block_json = serde_json::to_string(&val["result"]).unwrap();
+
+        block_json = block_json.replace("baseFeePerGas", "base_fee_per_gas");
+        block_json = block_json.replace("extraData", "extra_data");
+        block_json = block_json.replace("gasLimit", "gas_limit");
+        block_json = block_json.replace("gasUsed", "gas_used");
+        block_json = block_json.replace("logsBloom", "log_bloom");
+        block_json = block_json.replace("mixHash", "mix_hash");
+        block_json = block_json.replace("parentHash", "parent_hash");
+        block_json = block_json.replace("receiptsRoot", "receipts_root");
+        block_json = block_json.replace("sha3Uncles", "uncles_hash");
+        block_json = block_json.replace("stateRoot", "state_root");
+        block_json = block_json.replace("totalDifficulty", "total_difficulty");
+        block_json = block_json.replace("transactionsRoot", "transactions_root");
+        block_json = block_json.replace("parentHash", "parent_hash");
+        block_json = block_json.replace("miner", "author");
+
+        println!("block_json: {}", block_json);
+        let block_header: BlockHeader = serde_json::from_str(&block_json).unwrap();
+        println!("block_heade: {:?}", block_header);
+        Ok(BlockHeader)
     }
 }
 
@@ -34,9 +71,13 @@ impl Default for Eth1RPCClient {
 
 #[cfg(test)]
 mod tests {
+    use crate::eth1_rpc_client::Eth1RPCClient;
+
     const TEST_BEACON_BLOCK_ID: u32 = 741888;
 
     #[test]
-    fn test_get_header_from_json() {
+    fn test_smoke_get_block_by_number() {
+        let eth1_rpc_client = Eth1RPCClient::default();
+        eth1_rpc_client.get_block_header_by_number(766535);
     }
 }
