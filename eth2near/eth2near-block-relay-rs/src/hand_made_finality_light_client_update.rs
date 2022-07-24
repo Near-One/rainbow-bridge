f38424c8319f394482264819326a82214185a9fa
Olga Kunyavsksya <olga.kunyavskaya@aurora.dev>
2022-07-24 15:25:35 +0300

    get attested_header for finality light client update

--- eth2near/eth2near-block-relay-rs/src/hand_made_finality_light_client_update.rs
+++ eth2near/eth2near-block-relay-rs/src/hand_made_finality_light_client_update.rs
@@ -7,12 +7,15 @@ pub struct HandMadeFinalityLightClientUpdate {}
 impl HandMadeFinalityLightClientUpdate {
     pub fn get_finality_light_client_update(beacon_rpc_client: &BeaconRPCClient,
                                             attested_slot: u64) -> Result<LightClientUpdate, Box<dyn Error>> {
+        let attested_header = beacon_rpc_client.get_beacon_block_header_for_block_id(&format!("{}", attested_slot))?;
         Err("not implemented")?
     }
 }
 
 #[cfg(test)]
 mod tests {
+    const ATTESTED_HEADER_SLOT: u64 = 812637;
+
     #[test]
     fn test_hand_made_finality_light_client_update() {}
 }
