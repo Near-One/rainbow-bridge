67e0add6a0615d6bfa3177e7c113c331d137e3bb
Olga Kunyavsksya <olga.kunyavskaya@aurora.dev>
2022-07-26 12:33:45 +0300

    add two smoke tests

--- eth2near/finality-update-verify/src/lib.rs
+++ eth2near/finality-update-verify/src/lib.rs
@@ -6,9 +6,26 @@ pub fn is_correct_finality_update(network: &str, light_client_update: LightClien
 
 #[cfg(test)]
 mod tests {
+    use eth2_to_near_relay::beacon_rpc_client::BeaconRPCClient;
+    use crate::is_correct_finality_update;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn smoke_verify_finality_update_true() {
+        let network = "kiln";
+        let beacon_rpc_client = BeaconRPCClient::default();
+        let light_client_update_period_99 = beacon_rpc_client.get_light_client_update(99).unwrap();
+        let light_client_update_period_100 = beacon_rpc_client.get_light_client_update(100).unwrap();
+
+        assert!(is_correct_finality_update(network, light_client_update_period_100, light_client_update_period_99.sync_committee_update.unwrap().next_sync_committee));
+    }
+
+    #[test]
+    fn smoke_verify_finality_update_false() {
+        let network = "kiln";
+        let beacon_rpc_client = BeaconRPCClient::default();
+        let light_client_update_period_99 = beacon_rpc_client.get_light_client_update(99).unwrap();
+        let light_client_update_period_100 = beacon_rpc_client.get_light_client_update(100).unwrap();
+
+        assert!(!is_correct_finality_update(network, light_client_update_period_99, light_client_update_period_100.sync_committee_update.unwrap().next_sync_committee));
     }
 }
