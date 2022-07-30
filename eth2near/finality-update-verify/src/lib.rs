7bb2bf76fd2a7971d521a4ed0a624a25f508be89
Olga Kunyavsksya <olga.kunyavskaya@aurora.dev>
2022-07-30 08:49:37 +0300

    use aurora loghthouse

--- eth2near/finality-update-verify/src/lib.rs
+++ eth2near/finality-update-verify/src/lib.rs
@@ -72,23 +72,25 @@ mod tests {
     use eth2_to_near_relay::beacon_rpc_client::BeaconRPCClient;
     use crate::is_correct_finality_update;
 
+    const BEACON_ENDPOINT: &str = "https://lodestar-kiln.chainsafe.io";
+
     #[test]
     fn smoke_verify_finality_update_true() {
         let network = "kiln";
-        let beacon_rpc_client = BeaconRPCClient::default();
+        let beacon_rpc_client = BeaconRPCClient::new(BEACON_ENDPOINT);
         let light_client_update_period_99 = beacon_rpc_client.get_light_client_update(99).unwrap();
         let light_client_update_period_100 = beacon_rpc_client.get_light_client_update(100).unwrap();
 
-        assert!(is_correct_finality_update(network, light_client_update_period_100, light_client_update_period_99.sync_committee_update.unwrap().next_sync_committee));
+        assert!(is_correct_finality_update(network, &light_client_update_period_100, light_client_update_period_99.sync_committee_update.unwrap().next_sync_committee).unwrap());
     }
 
     #[test]
     fn smoke_verify_finality_update_false() {
         let network = "kiln";
-        let beacon_rpc_client = BeaconRPCClient::default();
+        let beacon_rpc_client = BeaconRPCClient::new(BEACON_ENDPOINT);
         let light_client_update_period_99 = beacon_rpc_client.get_light_client_update(99).unwrap();
         let light_client_update_period_100 = beacon_rpc_client.get_light_client_update(100).unwrap();
 
-        assert!(!is_correct_finality_update(network, light_client_update_period_99, light_client_update_period_100.sync_committee_update.unwrap().next_sync_committee));
+        assert!(!is_correct_finality_update(network, &light_client_update_period_99, light_client_update_period_100.sync_committee_update.unwrap().next_sync_committee).unwrap());
     }
 }
