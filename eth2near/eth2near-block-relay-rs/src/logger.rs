d6e07bd2dcd74565577ff172cad9b0982971ee61
Olga Kunyavsksya <olga.kunyavskaya@aurora.dev>
2022-07-30 09:08:20 +0300

    print error in stderr

--- eth2near/eth2near-block-relay-rs/src/logger.rs
+++ eth2near/eth2near-block-relay-rs/src/logger.rs
@@ -4,12 +4,16 @@ pub struct SimpleLogger;
 
 impl log::Log for SimpleLogger {
     fn enabled(&self, metadata: &Metadata) -> bool {
-        metadata.level() <= Level::Trace && metadata.target() == "relay"
+        metadata.target() == "relay"
     }
 
     fn log(&self, record: &Record) {
         if self.enabled(record.metadata()) {
-            println!("{}: {}", record.level(), record.args());
+            if record.metadata().level() <= Level::Warn { 
+                eprintln!("{}: {}", record.level(), record.args());
+            } else {
+                println!("{}: {}", record.level(), record.args());
+            }
         }
     }
 
