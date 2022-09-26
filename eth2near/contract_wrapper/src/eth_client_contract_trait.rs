9d34783b52e78f1b0b8735b795e3ff6046eb4bcd
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2022-09-26 15:38:28 +0300

    comments for EthClientContractTrait

--- eth2near/contract_wrapper/src/eth_client_contract_trait.rs
+++ eth2near/contract_wrapper/src/eth_client_contract_trait.rs
@@ -6,24 +6,47 @@ use near_sdk::Balance;
 use std::error::Error;
 use std::vec::Vec;
 
+/// Interface for using Ethereum Light Client
 pub trait EthClientContractTrait {
+    /// Returns the last submitted slot by this relay
     fn get_last_submitted_slot(&self) -> u64;
+
+    /// Check if block with the execution block hash is known in Eth Client
     fn is_known_block(&self, execution_block_hash: &H256) -> Result<bool, Box<dyn Error>>;
+
+    /// Submit the Light Client Update to Eth Client. Returns the Transaction Status
     fn send_light_client_update(
         &mut self,
         light_client_update: LightClientUpdate,
     ) -> Result<FinalExecutionOutcomeView, Box<dyn Error>>;
 
+    /// Get finalized beacon block hash from Eth Client
     fn get_finalized_beacon_block_hash(&self) -> Result<H256, Box<dyn Error>>;
+
+    /// Get finalized beacon block slot from Eth Client
     fn get_finalized_beacon_block_slot(&self) -> Result<u64, Box<dyn Error>>;
+
+    /// Send headers to Eth Client.
+    ///
+    /// # Arguments
+    ///
+    /// * `headers` - the list of headers for submission to Eth Client
+    /// * `end_slot` - the slot of the last header in list
     fn send_headers(
         &mut self,
         headers: &Vec<BlockHeader>,
         end_slot: u64,
     ) -> Result<FinalExecutionOutcomeView, Box<dyn std::error::Error>>;
 
+    /// Get the minimum deposit for registration new relay
     fn get_min_deposit(&self) -> Result<Balance, Box<dyn Error>>;
+
+    /// Register current relay on the Eth Client
     fn register_submitter(&self) -> Result<FinalExecutionOutcomeView, Box<dyn Error>>;
+
+    /// Check if the relay is registered on the Eth Client
     fn is_submitter_registered(&self, account_id: Option<AccountId>) -> Result<bool, Box<dyn Error>>;
+
+    /// Get the Light Client State of the Eth Client
     fn get_light_client_state(&self) -> Result<LightClientState, Box<dyn Error>>;
 }
