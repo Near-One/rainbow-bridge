210071ba83ac2df12bdedf8e78f11219b8a150ca
UrAvgDeveloper <adi.b20612@gmail.com>
2023-03-09 13:01:48 +0530

    fixed linting

--- eth2near/eth2near-block-relay/eth-proof-extractor.js
+++ eth2near/eth2near-block-relay/eth-proof-extractor.js
@@ -38,7 +38,7 @@ class EthProofExtractor {
       header_rlp: header.serialize(),
       account_rlp: account.serialize(),
       account_proof: proofFromWeb3.accountProof,
-      storage_proof: proofFromWeb3.storageProof[0].proof,
+      storage_proof: proofFromWeb3.storageProof[0].proof
     }
   }
 
@@ -142,7 +142,7 @@ async function ethToNearFindProof ({ lockedEventRaw, ethNodeUrl }) {
   web3.currentProvider.connection.close()
 }
 
-async function ethToNearFindStorageProof({ contractAddress, storageKey, blockNumber, ethNodeUrl }) {
+async function ethToNearFindStorageProof ({ contractAddress, storageKey, blockNumber, ethNodeUrl }) {
   const robustWeb3 = new RobustWeb3(ethNodeUrl)
   const web3 = robustWeb3.web3
   try {
@@ -160,7 +160,7 @@ async function ethToNearFindStorageProof({ contractAddress, storageKey, blockNum
       account_proof: extractedProof.account_proof.map(x => utils.stripHexPrefix(x)),
       expected_account_state: extractedProof.account_rlp.toString('hex'),
       storage_key_hash: utils.stripHexPrefix(web3.utils.keccak256(storageKey)),
-      storage_proof: extractedProof.storage_proof.map(x => utils.stripHexPrefix(x)),
+      storage_proof: extractedProof.storage_proof.map(x => utils.stripHexPrefix(x))
     }
     console.log(JSON.stringify(proof, JSONreplacer))
   } catch (error) {
