18481982cf6282a618adfe7c2762f1ea97081e06
karim-en <karim@aurora.dev>
2023-03-15 00:53:44 +0000

    Revert script changes

--- contracts/near/eth-client/test.sh
+++ contracts/near/eth-client/test.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-export ETH1_INFURA_API_KEY=<YOUR_KEY>
 # Run without default features
 RUST_BACKTRACE=1 cargo test --no-default-features --jobs 8 --package eth-client -- --nocapture
 # Run with default features
