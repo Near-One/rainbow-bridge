e17a5c331e6da34ecd702c4c8d4a8742850ff579
karim-en <karim.alabtakh@nearone.org>
2025-04-23 23:58:37 +0100

    Add target wasm32-unknown-unknown

--- contracts/near/eth2-client/test.sh
+++ contracts/near/eth2-client/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+rustup target add wasm32-unknown-unknown
 cargo near build non-reproducible-wasm --no-abi --no-default-features --features logs
 
 RUST_BACKTRACE=1 cargo test --jobs 8 --package eth2-client -- --nocapture
