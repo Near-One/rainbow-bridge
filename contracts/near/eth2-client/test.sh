fde2cffc553daea82739024a2f568e37fd2e0df2
r-near <163825889+r-near@users.noreply.github.com>
2025-05-12 17:59:07 -0700

    restore test code

--- contracts/near/eth2-client/test.sh
+++ contracts/near/eth2-client/test.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 rustup target add wasm32-unknown-unknown
 cargo near build non-reproducible-wasm --no-abi --no-default-features --features logs
-RUST_BACKTRACE=full cargo test --no-default-features --features bls,logs --package eth2-client -- tests::unit_tests::tests::generic_tests::test_gc_headers --nocapture
+RUST_BACKTRACE=1 cargo test --jobs 8 --package eth2-client -- --nocapture	
+
+RUST_BACKTRACE=1 cargo test --no-default-features --jobs 8 --package eth2-client -- --nocapture	
+
+RUST_BACKTRACE=1 cargo test --no-default-features --features bls --jobs 8 --package eth2-client -- --nocapture
