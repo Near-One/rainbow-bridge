a6b3df01af06c5ae2fe2f89ac98dfd11c923ff0a
r-near <163825889+r-near@users.noreply.github.com>
2025-05-25 09:01:29 -0700

    More cleanup

--- testing/transfer-eth-erc20/index.js
+++
@@ -1,7 +0,0 @@
-const { TransferETHERC20ToNear } = require('./to-near')
-const { TransferEthERC20FromNear } = require('./from-near')
-const { DeployToken } = require('./deploy-token')
-
-exports.TransferETHERC20ToNear = TransferETHERC20ToNear
-exports.TransferEthERC20FromNear = TransferEthERC20FromNear
-exports.DeployToken = DeployToken
