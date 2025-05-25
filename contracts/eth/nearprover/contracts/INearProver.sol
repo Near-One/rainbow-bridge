0bc3cc74557ef13fca27d501910e5b5be91d2163
r-near <163825889+r-near@users.noreply.github.com>
2025-05-25 08:59:01 -0700

    Cleanup

--- contracts/eth/nearprover/contracts/INearProver.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8;
-
-interface INearProver {
-    function proveOutcome(bytes calldata proofData, uint64 blockHeight) external view returns (bool);
-}
--- contracts/near/eth-client/test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Run with default features
-RUST_BACKTRACE=1 cargo test --jobs 1 --package eth-client -- --test-threads 1 --nocapture 
