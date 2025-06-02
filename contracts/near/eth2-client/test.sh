22f3db8ecad72c9b01e1b46e8c32d822dc0f1609
Karim <karim.alabtakh@nearone.org>
2025-06-03 00:21:21 +0100

    chore: remove bls flag and some getter (#947)
    * chore: remove bls flag and some getter
    
    * fix: issues found during review
    
    ---------
    
    Co-authored-by: Ivan Frolov <frolvanya@gmail.com>

--- contracts/near/eth2-client/test.sh
+++ contracts/near/eth2-client/test.sh
@@ -5,5 +5,3 @@ cargo near build non-reproducible-wasm --no-abi --no-default-features --features
 RUST_BACKTRACE=1 cargo test --jobs 8 --package eth2-client -- --nocapture
 
 RUST_BACKTRACE=1 cargo test --no-default-features --jobs 8 --package eth2-client -- --nocapture
-
-RUST_BACKTRACE=1 cargo test --no-default-features --features bls --jobs 8 --package eth2-client -- --nocapture
