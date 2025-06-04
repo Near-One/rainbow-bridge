a3e2efa49c482f359b1581b5a6bc5812c5fda8f3
r-near <163825889+r-near@users.noreply.github.com>
2025-06-04 14:36:53 -0700

    Remove eth2near

--- eth2near/eth_rpc_client/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod beacon_block_body_merkle_tree;
-pub mod beacon_rpc_client;
-pub mod config_for_tests;
-pub mod errors;
-pub mod eth1_rpc_client;
-pub mod execution_block_proof;
-pub mod light_client_snapshot_with_proof;
-pub mod utils;
--- eth2near/utilities/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod eth_proof_generator;
-pub mod eth_rpc_client;
-mod primitives;
-mod serde;
-mod types;
