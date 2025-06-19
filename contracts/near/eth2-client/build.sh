aa7ee6d5f23c12f583c8a98ad56c886375d52eb4
r-near <163825889+r-near@users.noreply.github.com>
2025-06-19 09:02:14 -0700

    Restore old build file

--- contracts/near/eth2-client/build.sh
+++ contracts/near/eth2-client/build.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
-rustup target add wasm32-unknown-unknown
-cargo near build non-reproducible-wasm
 
-cp ../target/near/eth2_client/eth2_client.wasm ../res/
+# Exit script as soon as a command fails.
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [[ -z "$BUILDKITE" ]] && [[ "$(uname -s)" != "Darwin" ]];
+then
+     userflag="-u $UID:$UID"
+else
+     userflag=""
+fi
+
+arch=`uname -m`
+if [ "$arch" == "arm64" ]
+then
+    tag=":latest-arm64"
+else
+    tag=""
+fi
+
+docker run \
+     --rm \
+     --mount type=bind,source=$DIR/..,target=/host \
+     --cap-add=SYS_PTRACE --security-opt seccomp=unconfined $userflag \
+     -w /host/eth2-client \
+     -e RUSTFLAGS='-C link-arg=-s' \
+     nearprotocol/contract-builder$tag \
+     /bin/bash -c "rustup target add wasm32-unknown-unknown; cargo build --target wasm32-unknown-unknown --release"
+
+cp $DIR/../target/wasm32-unknown-unknown/release/eth2_client.wasm $DIR/../res/
